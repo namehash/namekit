b2c39623177d9d9ae8a4aa54a4723df2ce8e9c27
Byczong <piotrzwsln8@gmail.com>
2023-08-28 21:39:50 +0200

    refactor, add custom errors

--- nameguard/nameguard.py
+++ nameguard/nameguard.py
@@ -1,3 +1,10 @@
+import httpx
+from fastapi import HTTPException
+from ens.constants import EMPTY_SHA3_BYTES
+from ens.utils import Web3
+from hexbytes import HexBytes
+from typing import Optional
+
 from label_inspector.inspector import Inspector
 from label_inspector.models import InspectorResultNormalized, InspectorResultUnnormalized, InspectorResult
 from label_inspector.config import initialize_inspector_config
@@ -13,8 +20,6 @@ from nameguard.models import (
     NameGuardSummary,
     NameStatus,
 )
-
-from nameguard.utils import normal_name_to_hash
 
 
 GRAPHEME_CHECKS = [
@@ -33,14 +38,110 @@ LABEL_CHECKS = [
 
 NAME_CHECKS = []
 
+ENS_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
+
+SUBGRAPH_NAME_QUERY = """
+query getDomains($nameHash: String) {
+  domain(id: $nameHash) {
+    id
+    labelhash
+    name
+    createdAt
+    parent {
+      id
+    }
+    resolver {
+      texts
+      address
+    }
+  }
+}
+"""  # redundant elements in query for future use
+
+# todo: move exceptions somewhere else?
+# -- exceptions --
+
+class InvalidNameHash(HTTPException):
+    def __init__(self, reason=''):
+        super().__init__(422, f"Provided namehash is not valid, reason: {reason}")
+
+class ENSSubgraphUnavailable(HTTPException):
+    def __init__(self, error_msg: str):  # todo: which status code?
+        super().__init__(418, f"Error while making request to ENS Subgraph: {error_msg}")
+
+class NamehashMismatchError(HTTPException):
+    def __init__(self):  # todo: which status code?
+        super().__init__(418, "Namehash calculated on the name returned from ENS Subgraph"
+                              " does not equal the input namehash.")
+
+class NamehashNotFoundInSubgraph(HTTPException):
+    def __init__(self):
+        super().__init__(404, "Provided namehash could not be found in ENS Subgraph.")
 
 def init_inspector():
     with initialize_inspector_config('prod_config') as config:
         return Inspector(config)
 
 
-def compute_namehash(name: str) -> str:
-    return normal_name_to_hash(name)
+def hexbytes_to_int(hb: HexBytes) -> int:
+    return int(hb.hex(), base=16)
+
+
+def int_to_hexstr(n: int, hex_len=64) -> str:
+    """
+    Given an integer `n`, return a hex-string prefixed with '0x',
+    padded with 0s to have exactly `hex_len` digits.
+
+    :raises ValueError  if `n` in hex repr has more digits than `hex_len`
+    """
+    res = f"{n:#0{hex_len + 2}x}"
+    if len(res) > hex_len + 2:
+        raise ValueError(f'Resulting hex number has more digits ({len(res) - 2}) than specified ({hex_len}).')
+    return res
+
+
+def namehash_from_name(name: str) -> str:
+    def label_to_hash(label_: str) -> HexBytes:
+        if "." in label_:
+            raise ValueError(f"Cannot generate hash for label {label_!r} with a '.'")
+        return Web3().keccak(text=label_)
+
+    node = EMPTY_SHA3_BYTES
+    if name:
+        labels = name.split(".")
+        for label in reversed(labels):
+            labelhash = label_to_hash(label)
+            assert isinstance(labelhash, bytes)  # todo: remove?
+            assert isinstance(node, bytes)
+            node = Web3().keccak(node + labelhash)
+    return node.hex()
+
+
+def validate_namehash(namehash: str) -> str:
+    """
+    Validate namehash string and return namehash in hex-string format.
+
+    :param namehash: A string representing a namehash. It can be in
+                     a) decimal format - decimal integer of any length,
+                     b) hex format - 64 hex digits prefixed with 0x.
+    :return: namehash in hex format (padded with 0s to match 64 digits and prefixed with 0x)
+    """
+    if namehash.startswith('0x'):
+        if len(namehash) != 66 or not all(c in '0123456789abcdefABCDEF' for c in namehash[2:]):
+            raise InvalidNameHash(reason="Hex number must be 64 digits long and prefixed with '0x'.")
+        return namehash
+    else:
+        try:
+            int_namehash = int(namehash)
+        except ValueError:
+            raise InvalidNameHash(
+                reason="Must be a valid, decimal integer or a hex number with 64 digits, prefixed with '0x'.")
+        try:
+            hex_namehash = int_to_hexstr(int_namehash)
+        except ValueError:
+            raise InvalidNameHash(
+                reason="The decimal integer converted to base-16 should have at most 64 digits.")
+        return hex_namehash
 
 
 def calculate_nameguard_rating(checks: list[GenericCheckResult]) -> Rating:
@@ -61,6 +162,7 @@ def agg_checks(checks: list[GenericCheckResult]) -> list[GenericCheckResult]:
 class NameGuard:
     def __init__(self):
         self.inspector = init_inspector()
+        self.httpx_client = httpx.AsyncClient()
 
     def inspect_name(self, name: str, return_labels=True) -> NameGuardResult:
         labels = name.split('.')
@@ -106,7 +208,7 @@ class NameGuard:
 
         return NameGuardResult(
             name=name,
-            namehash=compute_namehash(name),
+            namehash=namehash_from_name(name),
             status=NameStatus.NORMALIZED if name_normalized else NameStatus.UNNORMALIZED,
             summary=NameGuardSummary(
                 rating=calculate_nameguard_rating(name_checks),
@@ -153,3 +255,34 @@ class NameGuard:
             return InspectorResultNormalized(**result)
         else:
             return InspectorResultUnnormalized(**result)
+
+    async def namehash_to_normal_name_lookup(self, namehash_hexstr: str, network='mainnet') -> Optional[str]:
+        variables = {'nameHash': namehash_hexstr}
+
+        try:
+            response = await self.httpx_client.post(ENS_SUBGRAPH_URL + '?source=ens-nameguard',
+                                                    json={'query': SUBGRAPH_NAME_QUERY, 'variables': variables})
+            if response.status_code == 200:
+                response_json = response.json()
+            else:
+                raise ENSSubgraphUnavailable(
+                    f"Received unexpected status code from ENS Subgraph {response.status_code}: {response.text}")
+        except httpx.RequestError as ex:
+            raise ENSSubgraphUnavailable(f"RequestError has occurred: {ex}")
+
+        if 'data' not in response_json or 'domain' not in response_json['data']:
+            raise ValueError(f"Unexpected response body: {response_json}")  # todo: should it be ENSSubgraphUnavailable?
+        elif response_json == {'data': {'domain': None}}:
+            raise NamehashNotFoundInSubgraph()
+        elif 'name' in response_json['data']['domain']:
+            name = str(response_json['data']['domain']['name'])
+            if name.startswith('['):
+                print(f'Warning - unknown label: {name}')  # todo: create a logger?
+                return None
+            else:
+                calculated_namehash = namehash_from_name(name)
+                if calculated_namehash != namehash_hexstr:
+                    raise NamehashMismatchError()
+                return name
+        else:
+            raise ValueError(f"Unexpected response body: {response_json}")  # todo: should it be ENSSubgraphUnavailable?
--- nameguard/web_api.py
+++ nameguard/web_api.py
@@ -2,12 +2,10 @@ from enum import Enum
 from typing import Literal
 
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 
-from nameguard.nameguard import NameGuard
+from nameguard.nameguard import NameGuard, validate_namehash
 from nameguard.models import NameGuardResult, NameGuardBulkResult
-
-from utils import int_to_hexstr, namehash_to_normal_name_lookup
 
 
 class ApiVersion(str, Enum):
@@ -62,73 +60,29 @@ class InspectNamehashRequest(BaseModel):
     namehash: str
     network_name: Literal['mainnet']
 
-    @field_validator('namehash')
-    @classmethod
-    def valid_namehash_hex_or_int(cls, value: str) -> str:
-        if value.startswith('0x'):
-            if len(value) != 66 or not all(c in '0123456789abcdefABCDEF' for c in value[2:]):
-                raise ValueError("Must be a hex number with 64 digits, prefixed with 0x.")
-            return value
-        else:
-            try:
-                int_value = int(value)
-            except ValueError as ex:
-                raise ValueError(
-                    "Must be a valid, decimal integer or a hex number with 64 digits, prefixed with 0x.") from ex
-            try:
-                hex_value = int_to_hexstr(int_value)
-            except ValueError as ex:
-                raise ValueError(
-                    "The decimal integer represented as a hex number should have at most 64 digits.") from ex
-            return hex_value
-
 
 @app.post('/{api_version}/inspect-namehash')
-async def inspect_name(api_version: ApiVersion, request: InspectNamehashRequest) -> NameGuardResult:
-    if api_version != ApiVersion.V1:
-        raise Exception(f'API version {api_version} not supported')
-
-    network_name = request.network_name
-
-    name = request.namehash  # todo: implement (the same as below, just POST)
+async def inspect_namehash(api_version: ApiVersion, request: InspectNamehashRequest) -> NameGuardResult:
+    valid_namehash = validate_namehash(namehash=request.namehash)
+    name = await nameguard.namehash_to_normal_name_lookup(valid_namehash, network=request.network_name)
+    if name is None:
+        raise NotImplementedError()
     return nameguard.inspect_name(name)
 
 
 @app.get('/{api_version}/inspect-namehash/{network_name}/{namehash}')
-async def inspect_namehash_get(api_version: ApiVersion, network_name: str, namehash: str) -> NameGuardResult:
-    try:
-        valid_namehash = InspectNamehashRequest(namehash=namehash, network_name=network_name).namehash
-    except ValueError as v:
-        raise HTTPException(status_code=422, detail=str(v))
+async def inspect_namehash_get(
+        api_version: ApiVersion,
+        network_name: Literal['mainnet'],
+        namehash: str
+) -> NameGuardResult:
+    valid_namehash = validate_namehash(namehash=namehash)
+    name = await nameguard.namehash_to_normal_name_lookup(valid_namehash, network=network_name)
 
-    try:
-        name = await namehash_to_normal_name_lookup(valid_namehash, network=network_name)
-    except ValueError:
-        pass
-        # todo: For now, an unknown label should trigger a red NameGuard check result.
-        #  It also means that no grapheme level analysis for such a label will be possible.
-        #  This also means the “normalization” in the result should be “Unknown”.
-        name = valid_namehash
-
-    return await inspect_name(api_version, InspectNameRequest(name=name))
-
-# namehash
-#   Required
-#   String
-#   Must be a valid namehash.
-#   May be provided in either decimal or hex format.
-#   Interpreted as hex format if it begins with “0x”, else interpreted as decimal format.
-#   If interpreted in decimal format:
-#       The value must be a (possibly quite large) unsigned integer.
-#       Converting the value from an unsigned integer to a hex value must be successful.
-#       The resulting hex value must be padded with prefixing ‘0’ characters as needed to be exactly 64 hex digits long.
-#       If the resulting hex value is too large to fit within 64 hex digits then it is an error.
-#   If interpreted in hex format:
-#       Must be a value in the format: 0x[a-fA-F0-9]{64}
-
-
-# Please consider error cases that include:
-#   Unsupported API Version
-#   Unsupported Network Name
-#   Invalid NameHash
-#   NameHash Mismatch Error (see details below)
+    # todo: For now, an unknown label should trigger a red NameGuard check result.
+    #  It also means that no grapheme level analysis for such a label will be possible.
+    #  This also means the “normalization” in the result should be “Unknown”.
+    # for now, name is None if its an unknown label
+    if name is None:
+        raise NotImplementedError()
+    return nameguard.inspect_name(name)
