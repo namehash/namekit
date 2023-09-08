61ae9d2c1d665c195e0be3c00a70ba5f1d0d86da
Carbon225 <carbon225@protonmail.com>
2023-09-08 11:59:42 +0200

    BulkInspectNames

--- nameguard/web_api.py
+++ nameguard/web_api.py
@@ -38,12 +38,12 @@ async def inspect_name_post(api_version: ApiVersion, request: InspectNameRequest
 # -- bulk-inspect-name --
 
 
-class BulkInspectNameRequest(BaseModel):
+class BulkInspectNamesRequest(BaseModel):
     names: list[str] = Field(max_items=250)
 
 
 @app.post('/{api_version}/bulk-inspect-names')
-async def bulk_inspect_names(api_version: ApiVersion, request: BulkInspectNameRequest) -> NameGuardBulkResult:
+async def bulk_inspect_names(api_version: ApiVersion, request: BulkInspectNamesRequest) -> NameGuardBulkResult:
     return nameguard.bulk_inspect_names(request.names)
 
 
