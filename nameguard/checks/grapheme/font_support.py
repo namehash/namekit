4483f465c4877b20ff89074cef76644e45bb54a3
Carbon225 <carbon225@protonmail.com>
2023-09-11 19:51:11 +0200

    update font support check skip

--- nameguard/checks/grapheme/font_support.py
+++ nameguard/checks/grapheme/font_support.py
@@ -5,12 +5,20 @@ from nameguard.models import CheckStatus, Check, GenericCheckResult
 STATUS = CheckStatus.WARN
 MESSAGE_PASS = 'This grapheme is supported by common fonts'
 MESSAGE_FAIL = 'This grapheme is not supported by common fonts'
+MESSAGE_SKIP = 'It is unknown if this grapheme is supported by common fonts'
 
 
 def check_grapheme(grapheme: Grapheme) -> GenericCheckResult:
     passed = grapheme.font_support_all_os
-    return GenericCheckResult(
-        check=Check.FONT_SUPPORT,
-        status=CheckStatus.PASS if passed else STATUS,
-        message=MESSAGE_PASS if passed else MESSAGE_FAIL,
-    )
+    if passed is None:
+        return GenericCheckResult(
+            check=Check.FONT_SUPPORT,
+            status=CheckStatus.SKIP,
+            message=MESSAGE_SKIP,
+        )
+    else:
+        return GenericCheckResult(
+            check=Check.FONT_SUPPORT,
+            status=CheckStatus.PASS if passed else STATUS,
+            message=MESSAGE_PASS if passed else MESSAGE_FAIL,
+        )
