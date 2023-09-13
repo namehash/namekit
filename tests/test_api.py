e23cff5fa643c2c68c2d566caafdfad680d8e8c7
Krzysztof Wróbel <djstrong@gmail.com>
2023-09-13 13:43:19 +0200

    some tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 from fastapi.testclient import TestClient
 from urllib.parse import quote
@@ -34,6 +35,41 @@ def test_inspect_name_get(test_client, api_version):
     assert res_json['summary'] == {'highest_risk': None, 'rating': 'PASS', 'risk_count': 0}
     assert res_json['checks']
     assert res_json['labels']
+    assert all([label['labelhash'] for label in res_json['labels']])
+
+
+CORRECT_CHECKS_ORDER = ['ALERT', 'WARN', 'PASS', 'INFO', 'SKIP']
+
+
+def check_order_of_list(l: list[str]):
+    for i in range(len(l) - 1):
+        assert CORRECT_CHECKS_ORDER.index(l[i]) <= CORRECT_CHECKS_ORDER.index(
+            l[i + 1]), f"Order of checks is not correct: {l[i + 1]} should be before {l[i]}"
+
+
+def test_inspect_name_get_unnormalized(test_client, api_version):
+    name = 'bycz ong.eth'
+    response = test_client.get(f'/{api_version}/inspect-name/{name}')
+    assert response.status_code == 200
+    res_json = response.json()
+    pprint(res_json)
+
+    assert res_json['name'] == name
+    assert res_json['namehash'] == '0x3f97a45d7aa341b55b749b276f699278634ce4e5afa3e0753a8fe5be5d1355c2'
+    assert res_json['normalization'] == 'unnormalized'
+    assert res_json['summary']['rating'] == 'ALERT'
+    assert res_json['summary']['risk_count'] > 0
+    assert res_json['checks']
+    assert res_json['labels']
+    assert all([label['labelhash'] for label in res_json['labels']])  # labelhash for unnormalized
+    assert all([label['graphemes'] for label in res_json['labels']])  # graphemes for unnormalized
+
+    # TODO order of checks
+    # check_order_of_list([check['status'] for check in res_json['checks']])
+    # for label in res_json['labels']:
+    #     check_order_of_list([check['status'] for check in label['checks']])
+    #     for grapheme in label['graphemes']:
+    #         check_order_of_list([check['status'] for check in grapheme['checks']])
 
 
 @pytest.mark.parametrize(
@@ -46,7 +82,8 @@ def test_inspect_name_get(test_client, api_version):
         ('%2511%25.%3F.eth', '%11%.?.eth', True),
     ]
 )
-def test_inspect_name_get_special_characters(test_client, api_version, encoded_input_name: str, decoded_name: str, do_quote: bool):
+def test_inspect_name_get_special_characters(test_client, api_version, encoded_input_name: str, decoded_name: str,
+                                             do_quote: bool):
     if do_quote:
         encoded_input_name = quote(
             encoded_input_name.encode('utf-8'))  # because TestClient is doing additional unquote before sending request
@@ -82,8 +119,10 @@ def test_bulk_inspect_name_post(test_client, api_version):
     res_json = response.json()
     pprint(res_json)
 
-    assert all([all([key in r for key in ('name', 'namehash', 'normalization', 'summary')])
-                for r in res_json['results']])
+    # only these keys should be returned
+    assert all(
+        [set(r.keys()) == {'name', 'namehash', 'normalization', 'summary'} for r in res_json['results']]
+    )
 
     # more than 250 names
     response = test_client.post(f'/{api_version}/bulk-inspect-names', json={'names': names * 126})
@@ -111,18 +150,25 @@ def test_inspect_namehash_get(test_client, api_version, namehash: str, expected_
     assert res_json['namehash'] == '0xee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835'
     assert res_json['name'] == expected_name
 
+
 @pytest.mark.parametrize(
     "namehash, normalization, expected_name",
     [
-        ('0xe0fe380f4d877f643e88ceabbed4e5ee0efe66f079aabba23e8902336f7948da', 'unknown', '[af498306bb191650e8614d574b3687c104bc1cd7e07c522954326752c6882770].eth'),
-        ('0x0462571d34d206146958c44e473730b1b2630321072c7fbb92deeea946416dab', 'unknown', '[5bc926fc40cc7c49e0df6dddf26e4dc7b9d6d32f4a55d4f0670320dbf414afd2].byongdok.eth'),
-        ('0x5f57b185ab56ca42b5506f96694c767ebcc8c6e2854a79636b565e4ebe700fb0', 'unknown', '[2af8fae91ee5ef94f17f2c2f23532cc2d1ccaee78cae52efed0df04bc2463b13].[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
-        ('0xb2636b6e3b1abdd3fbec454d4f4b1a904e7b15e3609cb208bcfc5a5487293308', 'unknown', '[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
-        ('0x00f52438ae09d2f909ee2efc19ba8af75058e74ca4507aa091bd8282aa490e77', 'unknown', '[7710d5ebf94bcebcf1996bb7a3f5e24a6d24435b314b3cec815da03640c2940c].[2e8eaa68c7e128861299162323c29c29672f5c094aceaf22d9c0935e4bbd3f85].[a64d2b5a93eda272d27734cc2fb8d1c468562e279f1e97e759eea1a5a410f8e3].[462a1d6391f7ea5916874504f3b5fc8cd43626f6bbabc8a22fe4312dc1585362].enspunks.eth'),
+        ('0xe0fe380f4d877f643e88ceabbed4e5ee0efe66f079aabba23e8902336f7948da', 'unknown',
+         '[af498306bb191650e8614d574b3687c104bc1cd7e07c522954326752c6882770].eth'),
+        ('0x0462571d34d206146958c44e473730b1b2630321072c7fbb92deeea946416dab', 'unknown',
+         '[5bc926fc40cc7c49e0df6dddf26e4dc7b9d6d32f4a55d4f0670320dbf414afd2].byongdok.eth'),
+        ('0x5f57b185ab56ca42b5506f96694c767ebcc8c6e2854a79636b565e4ebe700fb0', 'unknown',
+         '[2af8fae91ee5ef94f17f2c2f23532cc2d1ccaee78cae52efed0df04bc2463b13].[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
+        ('0xb2636b6e3b1abdd3fbec454d4f4b1a904e7b15e3609cb208bcfc5a5487293308', 'unknown',
+         '[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
+        ('0x00f52438ae09d2f909ee2efc19ba8af75058e74ca4507aa091bd8282aa490e77', 'unknown',
+         '[7710d5ebf94bcebcf1996bb7a3f5e24a6d24435b314b3cec815da03640c2940c].[2e8eaa68c7e128861299162323c29c29672f5c094aceaf22d9c0935e4bbd3f85].[a64d2b5a93eda272d27734cc2fb8d1c468562e279f1e97e759eea1a5a410f8e3].[462a1d6391f7ea5916874504f3b5fc8cd43626f6bbabc8a22fe4312dc1585362].enspunks.eth'),
         ('0x1bc53f6413409d078ec18a29b17f981eafab341598a4e970ac9efab7d29258af', 'unnormalized', '[zzz].eth'),
     ]
 )
-def test_inspect_namehash_get_unknown(test_client, api_version, namehash: str, normalization: str, expected_name: str):
+def test_inspect_namehash_get_unknown_status(test_client, api_version, namehash: str, normalization: str,
+                                             expected_name: str):
     network_name = 'mainnet'
     response = test_client.get(f'/{api_version}/inspect-namehash/{network_name}/{namehash}')
     assert response.status_code == 200
@@ -130,6 +176,40 @@ def test_inspect_namehash_get_unknown(test_client, api_version, namehash: str, n
     pprint(res_json)
 
     assert res_json['normalization'] == normalization
+
+
+@pytest.mark.parametrize(
+    "namehash, expected_name",
+    [
+        ('0xe0fe380f4d877f643e88ceabbed4e5ee0efe66f079aabba23e8902336f7948da',
+         '[af498306bb191650e8614d574b3687c104bc1cd7e07c522954326752c6882770].eth'),
+        ('0x0462571d34d206146958c44e473730b1b2630321072c7fbb92deeea946416dab',
+         '[5bc926fc40cc7c49e0df6dddf26e4dc7b9d6d32f4a55d4f0670320dbf414afd2].byongdok.eth'),
+        ('0x5f57b185ab56ca42b5506f96694c767ebcc8c6e2854a79636b565e4ebe700fb0',
+         '[2af8fae91ee5ef94f17f2c2f23532cc2d1ccaee78cae52efed0df04bc2463b13].[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
+        ('0xb2636b6e3b1abdd3fbec454d4f4b1a904e7b15e3609cb208bcfc5a5487293308',
+         '[3fddf465ed81d79ae943b35800b1d187dc0b5d69614bf7e8ebddbae19d72cae8].genevaswis.eth'),
+        ('0x00f52438ae09d2f909ee2efc19ba8af75058e74ca4507aa091bd8282aa490e77',
+         '[7710d5ebf94bcebcf1996bb7a3f5e24a6d24435b314b3cec815da03640c2940c].[2e8eaa68c7e128861299162323c29c29672f5c094aceaf22d9c0935e4bbd3f85].[a64d2b5a93eda272d27734cc2fb8d1c468562e279f1e97e759eea1a5a410f8e3].[462a1d6391f7ea5916874504f3b5fc8cd43626f6bbabc8a22fe4312dc1585362].enspunks.eth'),
+    ]
+)
+def test_inspect_namehash_get_unknown(test_client, api_version, namehash: str, expected_name: str):
+    network_name = 'mainnet'
+    response = test_client.get(f'/{api_version}/inspect-namehash/{network_name}/{namehash}')
+    assert response.status_code == 200
+    res_json = response.json()
+    pprint(res_json)
+
+    assert res_json['normalization'] == 'unknown'
+    assert res_json['namehash']
+
+    # TODO labelhashes are available for every input
+    # assert all([label['labelhash'] for label in res_json['labels']])
+    # TODO label value for unknown labels is a labelhash
+    # for label in res_json['labels']:
+    #     if label['normalization'] == 'unknown':
+    #         assert re.match('^\[[0-9a0f]{64}\]$', label['label'])
+
 
 @pytest.mark.parametrize(
     "namehash, expected_status_code, expected_name",
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import pytest
 
 from nameguard.nameguard import (
     int_to_hexstr, namehash_from_name, validate_namehash, namehash_from_labelhash,
-    labelhash_from_label,  InvalidNameHash
+    labelhash_from_label, InvalidNameHash
 )
 
 
@@ -15,11 +15,19 @@ def test_int_to_hexstr():
         _ = int_to_hexstr(123456789**99)
 
 
-def test_namehash_from_name():
-    assert namehash_from_name('vitalik.eth') == \
-           '0xee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835'
-    assert namehash_from_name('byczong.eth') == \
-           '0xf8c2c01d386a4807b3ceb131e4975ff37b44824ac9307121b18223f3d77d0c2e'
+@pytest.mark.parametrize(
+    "name, namehash",
+    [
+        ('vitalik.eth', '0xee6c4522aab0003e8d14cd40a6af439055fd2577951148c14b6cea9a53475835'),
+        ('byczong.eth', '0xf8c2c01d386a4807b3ceb131e4975ff37b44824ac9307121b18223f3d77d0c2e'),
+        ('taoli.eth', '0x0000044a32f0964f4bf8fb4d017e230ad33595c0e149b6b2d0c34b733dcf906a'),  # leading zero
+        ('nIcK.eTh', '0xfcbdde3892e62a9ce1495d580fb7d43a8a7b493189aaa9ea72e9a755b355099a'),  # normalizable
+        ('&　󠁡�‍.eth', '0xade50cc78bd67478314cecd9b3623250de7b3d1d962e8df15b063fe365249c4b'),  # disallowed
+        ('', '0x0000000000000000000000000000000000000000000000000000000000000000'),
+    ]
+)
+def test_namehash_from_name(name, namehash):
+    assert namehash_from_name(name) == namehash
 
 
 def test_namehash_from_labelhash():
@@ -27,6 +35,7 @@ def test_namehash_from_labelhash():
            namehash_from_labelhash(labelhash_hexstr=labelhash_from_label('byczong'), parent_name='eth')
     assert namehash_from_name('iam.byczong.eth') == \
            namehash_from_labelhash(labelhash_hexstr=labelhash_from_label('iam'), parent_name='byczong.eth')
+
 
 def test_validate_namehash():
     nh = '0xf8c2c01d386a4807b3ceb131e4975ff37b44824ac9307121b18223f3d77d0c2e'
