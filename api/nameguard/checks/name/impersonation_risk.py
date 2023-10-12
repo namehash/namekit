302695ab4b7bdd6859b44e4c19cb00ce22dce6a7
Krzysztof Wróbel <djstrong@gmail.com>
2023-10-12 23:34:24 +0200

    context

--- api/nameguard/checks/name/impersonation_risk.py
+++ api/nameguard/checks/name/impersonation_risk.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from nameguard.models import CheckStatus, Check, GenericCheckResult
+from nameguard.context import endpoint_name
 from label_inspector.models import InspectorResult
 
 
@@ -8,6 +9,7 @@ MESSAGE_PASS = 'Name shows no signs of impersonation'
 MESSAGE_FAIL = 'Name might be an impersonation of `{}`'
 MESSAGE_SKIP_UNK = 'Name contains unknown labels and cannot be checked for impersonation risk'
 MESSAGE_SKIP_CANON = 'Name contains labels with unknown canonical forms and cannot be checked for impersonation risk'
+MESSAGE_FAIL_OTHER = 'Name may receive potential impersonation warnings'
 
 
 def check_name(labels: list[Optional[InspectorResult]]) -> GenericCheckResult:
@@ -27,8 +29,17 @@ def check_name(labels: list[Optional[InspectorResult]]) -> GenericCheckResult:
     name = '.'.join(label.label for label in labels)
     canonical = '.'.join(canonicals)
     passed = name == canonical
+
+    if passed:
+        message = MESSAGE_PASS
+    else:
+        if endpoint_name.get() == 'primary-name':
+            message = MESSAGE_FAIL.format('.'.join(label.beautiful_canonical_label for label in labels))
+        else:
+            message = MESSAGE_FAIL_OTHER
+    
     return GenericCheckResult(
         check=Check.IMPERSONATION_RISK,
         status=CheckStatus.PASS if passed else STATUS,
-        message=MESSAGE_PASS if passed else MESSAGE_FAIL.format('.'.join(label.beautiful_canonical_label for label in labels)),
+        message=message,
     )
