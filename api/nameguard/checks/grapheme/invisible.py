f0290099e229f15045aeac786f45274b2a7f21dc
Carbon225 <carbon225@protonmail.com>
2023-10-24 21:52:39 +0200

    update invisible message

--- api/nameguard/checks/grapheme/invisible.py
+++ api/nameguard/checks/grapheme/invisible.py
@@ -5,8 +5,8 @@ from nameguard.models import CheckStatus, Check, GenericCheckResult, GraphemeChe
 STATUS = CheckStatus.ALERT
 
 G_MESSAGE_PASS = 'This grapheme is visible'
-L_MESSAGE_PASS = 'This label is fully visible'
-N_MESSAGE_PASS = 'This name is fully visible'
+L_MESSAGE_PASS = 'This label does not contain invisible graphemes'
+N_MESSAGE_PASS = 'This name does not contain invisible graphemes'
 
 G_MESSAGE_FAIL = 'This grapheme is invisible'
 L_MESSAGE_FAIL = 'This label contains invisible graphemes'
