f831bac58be7fe88cd14e4e66dc66a12068c1cf0
Jamie Barton <jamie@notrab.dev>
2023-10-26 15:40:02 +0100

    remove unused export

--- packages/nameguard-react/src/index.ts
+++ packages/nameguard-react/src/index.ts
@@ -1,7 +1,6 @@
 export { useInspectName } from "./use-inspect-name";
 
 export { Report } from "./Report";
-export { Summary } from "./Summary";
 export { Shield } from "./Shield";
 export { Check } from "./Check";
 export { Tooltip } from "./Tooltip";
