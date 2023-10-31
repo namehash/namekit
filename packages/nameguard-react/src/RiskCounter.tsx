deb80376203c9c6d6e4729e9498014d03595d7d1
Jamie Barton <jamie@notrab.dev>
2023-10-31 14:52:53 +0000

    fix flex ordering for grapheme cards (#88)
    * fix flex ordering
    
    * move component

---
+++ packages/nameguard-react/src/RiskCounter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ConsolidatedGraphemeGuardReport } from "@namehash/nameguard";
+
+export function RiskCounter({
+  count,
+}: {
+  count: ConsolidatedGraphemeGuardReport["risk_count"];
+}) {
+  if (count === 0 || count === 1) return null;
+
+  return (
+    <span className="bg-yellow-100 rounded-full px-3 py-0.5 text-yellow-800 text-xs md:text-sm font-medium">
+      + {count - 1} more risk{count - 1 >= 2 && "s"}
+    </span>
+  );
+}
