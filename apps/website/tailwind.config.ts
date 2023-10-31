3bcc60c2a9b1750147158e41d681cb50e602f784
Jędrzej Ziebura <ziebura.jedrzej@gmail.com>
2023-10-31 16:02:05 +0100

    Landing Page - DRAFT (#70)
    * Ready feature section in progress
    
    * Slight style adjustment on ReadySection + ComingSoon section + very beginning of footer
    
    * Finished Footer + whole Header and ExitSection + QuickStartSection without nameguard 'crests' + beginnings of RoadMap
    
    * RoadMap section is ready for desktop version + small fixes for vercel build
    
    * Goal Priority 1 - done
    
    * Extra minor fix - check if build passes without Search
    
    * add tests section to readme
    
    * -||-
    
    * change pip to poetry
    
    * fix poetry install
    
    * trying if the deployment builds, 'EMFILE: too many files open recreation' try
    
    * feat(packages): nameparser (#80)
    
    * remove includes
    
    * feat(packages): nameparser
    
    * chore: bump versions
    
    * chore: lint
    
    * chore: lint (#81)
    
    * lint
    
    * lint
    
    * lint
    
    * Goal Priority 2 - hover states + clipboard + beginning of mobile layout
    
    * Goal Priority 2: mobile layout - header, footer, hero, ready & exit sections ready
    
    * Goal Priority 2 - mobile layout is ready + SEO metadata from Goal Priority 3
    
    * Desktop layout improvements
    
    ---------
    
    Co-authored-by: Byczong <piotrzwsln8@gmail.com>
    Co-authored-by: Jamie Barton <jamie@notrab.dev>

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -9,10 +9,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+          "figma-black": "#201F1F",
+          "code-gray": "#434446",
+      },
+      padding: {
+        "7.5": "30px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "purple_background": "radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.6) 0%, #FFF 100%), linear-gradient(180deg, #FFAF00 0%, #F112D9 32.29%, #4C3FA0 70.83%, #2ED3C6 95.83%)",
+        "green_background": "radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.6) 0%, #FFF 100%), linear-gradient(180deg, #FDC46A 0%, #2ED3C6 32.29%, #6DFFB7 70.83%, #6DFFB7 95.83%)",
+        "purple_background_sm": "radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.8) 0%, #FFF 100%), linear-gradient(180deg, #FFAF00 0%, #F112D9 32.29%, #4C3FA0 70.83%, #2ED3C6 95.83%)",
+        "green_background_sm": "radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.8) 0%, #FFF 100%), linear-gradient(180deg, #FDC46A 0%, #2ED3C6 32.29%, #6DFFB7 70.83%, #6DFFB7 95.83%)",
+        "exit_section_background" : "linear-gradient(0deg, rgba(255, 255, 255, 0.85) 2.25%, #FFF 96.18%), linear-gradient(90deg, #FFAF00 0%, #F112D9 32.29%, #4C3FA0 70.83%, #2ED3C6 95.83%)",
+        "hero_background": "linear-gradient(180deg, rgba(249, 250, 251, 0.00) 0%, #F9FAFB 100%);",
+      },
+      screens: {
+        'gt_mobile': '376px',
+        'desktop_br': '800px',
+        'hero_br': '1150px',
+      },
+      fontSize: {
+        footer_text: ['12px', '20px'],
       },
     },
   },
