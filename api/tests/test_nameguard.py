8ac7970d265606e821edafef55cac98fbba43267
Carbon <carbon225@protonmail.com>
2023-11-02 23:45:17 +0100

    update test

--- api/tests/test_nameguard.py
+++ api/tests/test_nameguard.py
@@ -350,5 +350,5 @@ def test_generic_check_result_operators():
 
 
 def test_generic_check_result_repr():
-    assert repr(GenericCheckResult(**{'check': Check.NORMALIZED, 'status': CheckStatus.PASS, '_name_message': ''})) == \
+    assert repr(GenericCheckResult(check=Check.NORMALIZED, status=CheckStatus.PASS, _name_message='')) == \
            'normalized(pass)'
