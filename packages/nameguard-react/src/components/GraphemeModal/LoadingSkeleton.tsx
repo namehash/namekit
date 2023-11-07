9b91943e9606f1ad22dc4a99d761245ac0411d81
Jamie Barton <jamie@notrab.dev>
2023-11-07 08:46:14 +0000

    svgr

--- packages/nameguard-react/src/components/GraphemeModal/LoadingSkeleton.tsx
+++ packages/nameguard-react/src/components/GraphemeModal/LoadingSkeleton.tsx
@@ -13,8 +13,8 @@ export const LoadingSkeleton = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M9.48428 1.1698C9.77353 0.895873 10.2265 0.895873 10.5157 1.1698C12.5325 3.07977 15.2538 4.25011 18.25 4.25011C18.2977 4.25011 18.3453 4.24981 18.3928 4.24922C18.7202 4.24515 19.0123 4.4539 19.1146 4.76491C19.5271 6.01968 19.75 7.35963 19.75 8.75015C19.75 14.6922 15.6859 19.6831 10.1869 21.0984C10.0643 21.13 9.93569 21.13 9.81306 21.0984C4.31406 19.6831 0.25 14.6922 0.25 8.75015C0.25 7.35963 0.472873 6.01968 0.885415 4.76491C0.987669 4.4539 1.27984 4.24515 1.60721 4.24922C1.65473 4.24981 1.70233 4.25011 1.75 4.25011C4.74624 4.25011 7.46752 3.07977 9.48428 1.1698Z"
               fill="url(#paint0_linear_1388_4437)"
             />
@@ -27,8 +27,8 @@ export const LoadingSkeleton = () => {
                 y2="2.90111"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stop-color="#D9DDE6" />
-                <stop offset="1" stop-color="#F0F4FB" />
+                <stop stopColor="#D9DDE6" />
+                <stop offset="1" stopColor="#F0F4FB" />
               </linearGradient>
             </defs>
           </svg>
