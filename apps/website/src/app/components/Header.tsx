14125284fb047d20006b483f70c1b544965f9deb
Y3drk <ziebura.jedrzej@gmail.com>
2023-11-13 14:38:03 +0100

    error 404 page without search modal

---
+++ apps/website/src/app/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { GithubLogo } from "@/app/components/icons/GithubLogo";
+import { Search } from "@namehash/nameguard-react";
+
+export const Header = () => {
+  return (
+    <header className="relative top-0 w-full z-10 border-b border-gray-300 box-border inline-flex h-[56px] px-[20px] py-[9px] items-center justify-between gt_mobile:flex gt_mobile:flex-row gt_mobile:h-[70px] md:px-28 gt_mobile:py-4">
+      <div className="flex flex-row gap-7 justify-between items-center">
+        <div className="flex flex-row justify-between items-center gap-1 cursor-pointer">
+          <a href={"https://nameguard.io"}>
+            <p className="text-black not-italic font-bold text-[22.683px] leading-[22.683px] tracking-[-0.907px] gt_mobile:text-[27.816px] gt_mobile:leading-[27.816px] gt_mobile:tracking-[-1.113px]">
+              NameGuard
+            </p>
+          </a>
+          <a href={"https://nameguard.io"}>
+            <div className="relative -top-1.5 bg-black w-fit h-fit p-[2.8px] rounded-[2.8px] flex-shrink-0">
+              <p className="text-white not-italic font-semibold pb-[0.5px] text-[6.857px] leading-[7.619px] gt_mobile:text-[8.409px] gt_mobile:leading-[9.343px]">
+                beta
+              </p>
+            </div>
+          </a>
+        </div>
+        <div className="hidden flex search_bar_change:block gt_mobile:w-full gt_mobile:h-full gt_mobile:p-0 gt_mobile:m-0">
+          <Search />
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-end gap-0 gt_mobile:justify-between gt_mobile:gap-1 md:gap-5">
+        <a
+          className="px-[10px] py-[9px] not-italic text-black font-medium text-sm leading-5 rounded-md gt_mobile:hover:bg-gray-100"
+          href={"https://api.nameguard.io/docs"}
+        >
+          Docs
+        </a>
+        <a
+          className="rounded-md gt_mobile:hover:bg-gray-100"
+          href={"https://github.com/namehash/nameguard"}
+        >
+          <div className="flex flex-row justify-between items-center gap-2 py-[7px] px-[10px]">
+            <GithubLogo />
+            <p className="not-italic text-black font-medium text-sm leading-5">
+              Github
+            </p>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
