3f476808933b459c7726f79236583c7556794bbc
kwrobel.eth <djstrong@gmail.com>
2023-11-15 20:56:00 +0100

    Update api/nameguard/checks/name/namewrapper_fuses.py
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- api/nameguard/checks/name/namewrapper_fuses.py
+++ api/nameguard/checks/name/namewrapper_fuses.py
@@ -3,7 +3,7 @@ from nameguard.models import CheckStatus, Check, GenericCheckResult, NameCheckRe
 from label_inspector.models import InspectorResult
 
 
-MESSAGE_INFO = 'We are working on checking NameWrapper fuses'
+MESSAGE_INFO = 'NameWrapper fuse checks will be added soon'
 
 
 def check_name(labels: list[Optional[InspectorResult]]) -> GenericCheckResult:
