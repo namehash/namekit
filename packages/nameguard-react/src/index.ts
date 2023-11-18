b21cc6e5fd810514321f100ba83eb9f5d56860e5
Jamie Barton <jamie@notrab.dev>
2023-11-18 15:50:17 +0000

    update react exports

--- packages/nameguard-react/src/index.ts
+++ packages/nameguard-react/src/index.ts
@@ -1,7 +1,10 @@
 export { Tooltip } from "./components/Tooltip/Tooltip";
 export { Search } from "./components/Search/Search";
 export { Report } from "./components/Report/Report";
+export { SearchModal } from "./components/Search/SearchModal";
+export { SettingsModal } from "./components/SettingsModal/SettingsModal";
 export { useChatModalStore } from "./stores/chat";
+export { useSearchStore } from "./stores/search";
 
 export { NameBadge } from "./components/NameBadge/NameBadge";
 export { Shield } from "./components/Shield/Shield";
