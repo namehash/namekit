43fad09f224aafb5ebf1063314c089349e8b1e7c
Jamie Barton <jamie@notrab.dev>
2023-11-30 08:20:56 +0000

    feat(nameguard-react): canonical badge (#170)
    * feat: canonical badge
    
    * refactor: canonical badge
    
    * bump deps

--- packages/nameguard-react/src/index.ts
+++ packages/nameguard-react/src/index.ts
@@ -7,4 +7,5 @@ export { useChatModalStore } from "./stores/chat";
 export { useSearchStore } from "./stores/search";
 
 export { NameBadge } from "./components/NameBadge/NameBadge";
+export { NameShield } from "./components/NameShield";
 export { Shield } from "./components/Shield/Shield";
