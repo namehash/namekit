5cdc7befedb82a924de201cd3531e8aa643356a1
Jamie Barton <jamie@notrab.dev>
2023-11-30 08:22:15 +0000

    Update packages/nameguard-react/src/components/Share/Share.tsx
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- packages/nameguard-react/src/components/Share/Share.tsx
+++ packages/nameguard-react/src/components/Share/Share.tsx
@@ -122,7 +122,7 @@ export function Share({ name }: ShareProps) {
                           <path d="M13.5222 10.7749L19.4785 4H18.0671L12.8952 9.88256L8.76437 4H4L10.2466 12.8955L4 20H5.41155L10.8732 13.7878L15.2356 20H20L13.5218 10.7749H13.5222ZM11.5889 12.9738L10.956 12.0881L5.92015 5.03974H8.0882L12.1522 10.728L12.7851 11.6137L18.0677 19.0075H15.8997L11.5889 12.9742V12.9738Z" />
                         </svg>
                       </div>
-                      <span className="text-black text-xs">Twitter</span>
+                      <span className="text-black text-xs">X / Twitter</span>
                     </a>
                     {/* <a
                       href="https://twitter.com"
