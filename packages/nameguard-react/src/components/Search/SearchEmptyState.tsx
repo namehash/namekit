953db86353511a7a1e91debff639551a8c48afdd
Jamie Barton <jamie@notrab.dev>
2024-01-24 09:20:06 +0000

    add scrolling shadow overlay

--- packages/nameguard-react/src/components/Search/SearchEmptyState.tsx
+++ packages/nameguard-react/src/components/Search/SearchEmptyState.tsx
@@ -51,7 +51,7 @@ export const SearchEmptyState = () => {
     const checkScrollPosition = () => {
       if (sliderRef.current) {
         const { scrollLeft, scrollWidth, clientWidth } = sliderRef.current;
-        setIsAtStart(scrollLeft <= 0);
+        setIsAtStart(scrollLeft <= 60);
         setIsAtEnd(scrollLeft + clientWidth >= scrollWidth);
       }
     };
@@ -89,44 +89,51 @@ export const SearchEmptyState = () => {
         </p>
       </div>
 
-      <div
-        className="max-w-2xl mx-auto relative z-10 overflow-x-scroll h-16 scrollbar-hide whitespace-nowrap scroll-smooth w-full"
-        ref={sliderRef}
-      >
-        <div className="inline-flex items-center space-x-1 absolute left-0 w-full">
-          {!isAtStart && (
-            <div className="md:hidden fixed left-0 z-50">
-              <button
-                className="appearance-none p-2 bg-white flex items-center justify-center"
-                onClick={slideLeft}
-              >
-                <ChevronLeftIcon className="fill-current text-black w-5 h-5" />
-              </button>
-            </div>
-          )}
-          {!isAtEnd && (
-            <div className="md:hidden fixed right-0 z-50">
-              <button
-                className="appearance-none p-2 flex items-center justify-center bg-gradient-to-l from-white via-white to-transparent"
-                onClick={slideRight}
-              >
-                <ChevronRightIcon className="fill-current text-black w-5 h-5" />
-              </button>
-            </div>
-          )}
-          {isLoading &&
-            examples.map((e, index) => (
-              <NameBadge placeholder={e} key={index} />
+      <div className="relative max-w-3xl mx-auto h-9 w-full">
+        <div className="z-10 absolute top-0 z-20 inset-x-0 w-full h-full pointer-events-none shadow-[inset_45px_0_25px_-20px_rgba(255,255,255,0.97),inset_-45px_0_25px_-20px_rgba(255,255,255,0.97)]"></div>
+        <div
+          className="relative z-10 overflow-x-scroll scrollbar-hide whitespace-nowrap scroll-smooth w-full h-full"
+          ref={sliderRef}
+        >
+          <div className="inline-flex items-center space-x-1 absolute left-0 w-full pl-6 pr-10">
+            {!isAtStart && (
+              <div className="md:hidden fixed left-0 z-50">
+                <button
+                  className="appearance-none p-2 bg-white flex items-center justify-center"
+                  onClick={slideLeft}
+                >
+                  <ChevronLeftIcon className="fill-current text-black w-5 h-5" />
+                </button>
+              </div>
+            )}
+            {!isAtEnd && (
+              <div className="md:hidden fixed right-0 z-50">
+                <button
+                  className="appearance-none p-2 flex items-center justify-center bg-gradient-to-l from-white via-white to-transparent"
+                  onClick={slideRight}
+                >
+                  <ChevronRightIcon className="fill-current text-black w-5 h-5" />
+                </button>
+              </div>
+            )}
+            {isLoading &&
+              examples.map((e, index) => (
+                <NameBadge placeholder={e} key={index} />
+              ))}
+            {data?.results?.map((report, index) => (
+              <NameBadge
+                key={index}
+                data={report}
+                onClick={() => openModal(report.name)}
+              />
             ))}
-          {data?.results?.map((report, index) => (
-            <NameBadge
-              key={index}
-              data={report}
-              onClick={() => openModal(report.name)}
-            />
-          ))}
+            <div className="w-5 flex-shrink-0 relative">
+              <span className="sr-only"></span>
+            </div>
+          </div>
         </div>
       </div>
+
       <div className="fixed inset-0 z-[5] h-full w-full bg-[radial-gradient(#DDDDDD_1px,transparent_1px)] [background-size:24px_24px] opacity-70"></div>
     </div>
   );
