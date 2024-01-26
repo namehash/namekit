5ac5633f6faa806329ffceeeeedab5826710d898
frankind.eth <faguzzi3@gmail.com>
2024-01-26 14:22:11 -0300

    feat: [SC-23150] Updated @namehash/ens-webfont package.json configs

--- packages/fonts/tsup.config.js
+++ packages/fonts/tsup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["index.ts"],
   format: ["esm", "cjs"],
   splitting: true,
   sourcemap: true,
