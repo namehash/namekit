5e19b23b1662b12a862328e68bd00bedb99e0142
Jędrzej Ziebura <ziebura.jedrzej@gmail.com>
2024-01-28 15:30:13 +0100

    Feature-23173:  Add search icon to mobile menu (#210)
    * feat: homepage carousel
    
    * Update of the footer + footer links now open in a new tab
    
    * Visuals for search icon in mobile header
    
    * Revert of Search icon changes to be moved to other branch
    
    * Icon name adjustment
    
    * Adding search icon to mobile menu + investigation of og-image links
    
    * more tests of og images links
    
    * Minor commit to trigger Vercel Deploy. Preview
    
    * Update coverage badge
    
    ---------
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    Co-authored-by: kwrobel.eth <djstrong@gmail.com>
    Co-authored-by: github-actions <actions@github.com>

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Favicon from "../../public/favicon/favicon_package_v0-2/favicon.ico";
 import AppleTouchIcon from "../../public/favicon/favicon_package_v0-2/apple-touch-icon.png";
-import og_img from "../../public/openGraph/og-image.png";
-import og_image_twitter from "../../public/openGraph/og-image-twitter.png";
 import { NameGuardWrapper } from "./components/NameGuardWrapper";
 
 import { Footer } from "@/app/components/Footer";
@@ -15,6 +13,7 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nameguard.io"),
   title: "NameGuard - Protect your community with NameGuard for ENS",
   description:
     "Guard your users from heartbreak and encourage best practice usage of ENS.",
@@ -30,7 +29,7 @@ export const metadata: Metadata = {
       "Guard your users from heartbreak and encourage best practice usage of ENS.",
     url: "https://nameguard.io",
     images: {
-      url: og_img.src,
+      url: "https://nameguard.io/openGraph/og-image.png",
       alt: "NameGuard - Protect your community with NameGuard for ENS",
     },
   },
@@ -41,7 +40,7 @@ export const metadata: Metadata = {
     description:
       "Guard your users from heartbreak and encourage best practice usage of ENS.",
     images: {
-      url: og_image_twitter.src,
+      url: "https://nameguard.io/openGraph/og-image-twitter.png",
       alt: "NameGuard - Protect your community with NameGuard for ENS",
     },
   },
