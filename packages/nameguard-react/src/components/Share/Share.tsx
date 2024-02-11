18ad0cfdc6b384e8821605cb581a306b0bba318e
Jamie Barton <jamie@notrab.dev>
2024-02-11 11:09:39 +0000

    toast style changes

--- packages/nameguard-react/src/components/Share/Share.tsx
+++ packages/nameguard-react/src/components/Share/Share.tsx
@@ -1,6 +1,7 @@
 import React, { useState, Fragment } from "react";
 import { Transition, Dialog } from "@headlessui/react";
 import { XMarkIcon, ArrowUpTrayIcon } from "@heroicons/react/24/solid";
+
 import { toast } from "sonner";
 
 import { Tooltip } from "../Tooltip/Tooltip";
@@ -14,7 +15,7 @@ function createTwitterLink(name: string) {
   const url = `https://nameguard.io/inspect/${encodeURIComponent(name)}`;
 
   return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-    tweetText,
+    tweetText
   )}&url=${encodeURIComponent(url)}`;
 }
 
@@ -36,17 +37,34 @@ export function Share({ name }: ShareProps) {
 
   const twitterLink = createTwitterLink(name);
   const telegramLink = createTelegramLink(
-    `https://nameguard.io/inspect/${encodeURIComponent(name)}`,
+    `https://nameguard.io/inspect/${encodeURIComponent(name)}`
   );
   const emailLink = createMailToLink(
     `NameGuard Report for ${name}`,
-    `Check this out!\nhttps://nameguard.io/inspect/${encodeURIComponent(name)}`,
+    `Check this out!\nhttps://nameguard.io/inspect/${encodeURIComponent(name)}`
   );
   const copyLinkToClipboard = () => {
     navigator.clipboard.writeText(
-      `https://nameguard.io/inspect/${encodeURIComponent(name)}`,
+      `https://nameguard.io/inspect/${encodeURIComponent(name)}`
     );
-    toast("Link copied to clipboard");
+    toast.success("Link copied to clipboard", {
+      icon: (
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM11.7071 6.70711C12.0976 6.31658 12.0976 5.68342 11.7071 5.29289C11.3166 4.90237 10.6834 4.90237 10.2929 5.29289L7 8.58579L5.70711 7.29289C5.31658 6.90237 4.68342 6.90237 4.29289 7.29289C3.90237 7.68342 3.90237 8.31658 4.29289 8.70711L6.29289 10.7071C6.68342 11.0976 7.31658 11.0976 7.70711 10.7071L11.7071 6.70711Z"
+            fill="#34D399"
+          />
+        </svg>
+      ),
+    });
     closeModal();
   };
 
