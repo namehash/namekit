2f0508172499da529bb01029fa6373f9eaaf5eee
Jamie Barton <jamie@notrab.dev>
2024-02-13 08:41:34 +0000

    Notrab/sc 24124/fix layout shift on pills in nameguard (#226)
    * add ens-font to namebadge placeholder
    
    * use CheckCircleIcon
    
    * fix medium shields
    
    * Loading shield animate (#227)
    
    * add animate-pulse to loading shields
    
    * add animate-pulse to loading shields

--- packages/nameguard-react/src/components/icons/Loading.tsx
+++ packages/nameguard-react/src/components/icons/Loading.tsx
@@ -7,6 +7,7 @@ export const LoadingIcon = (props: any) => (
     viewBox="0 0 14 14"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    className="animate-pulse"
     {...props}
   >
     <path
