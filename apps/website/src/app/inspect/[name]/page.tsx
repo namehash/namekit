e1261b552f4139d21138aeafc1bf89657c231774
Jamie Barton <jamie@notrab.dev>
2024-04-04 13:12:32 +0100

    feat: set title for inspect page

--- apps/website/src/app/inspect/[name]/page.tsx
+++ apps/website/src/app/inspect/[name]/page.tsx
@@ -1,13 +1,32 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { nameguard } from "@namehash/nameguard";
 
 import { NGReport } from "@/app/components/NGReport";
 
-export default async function Namekit({
-  params,
-}: {
+type Props = {
   params: { name: string };
-}) {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { name } = params;
+
+  if (!name) return notFound();
+
+  const decodedName = decodeURIComponent(name);
+
+  return {
+    title: `${decodedName} - NameGuard Report`,
+    twitter: {
+      title: `${decodedName} - NameGuard Report`,
+    },
+    openGraph: {
+      title: `${decodedName} - NameGuard Report`,
+    },
+  };
+}
+
+export default async function Namekit({ params }: Props) {
   const { name } = params;
 
   if (!name) return notFound();
