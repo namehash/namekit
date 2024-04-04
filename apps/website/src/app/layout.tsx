b30df7b4858707e144a304e03e51dac2b21f3030
Jamie Barton <jamie@notrab.dev>
2024-04-04 12:35:58 +0100

    feat: use namehash/utils metadata

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "@namehash/ens-webfont";
 import type { Metadata } from "next";
+import { Metadata as NamehashMetadata } from "@namehash/utils";
 import { Inter } from "next/font/google";
 import Favicon from "../../public/favicon/favicon_package_v0-2/favicon.ico";
 import AppleTouchIcon from "../../public/favicon/favicon_package_v0-2/apple-touch-icon.png";
@@ -14,37 +15,30 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 
+const title = "NameGuard - Protect your community with NameGuard for ENS";
+const description =
+  "Guard your users from heartbreak and encourage best practice usage of ENS.";
+
 export const metadata: Metadata = {
+  ...NamehashMetadata.defaultMetdata,
   metadataBase: new URL("https://nameguard.io"),
-  title: "NameGuard - Protect your community with NameGuard for ENS",
-  description:
-    "Guard your users from heartbreak and encourage best practice usage of ENS.",
+  title,
+  description,
   keywords: ["nameguard", "normalization", "ens", "web3", "eth"],
   icons: [
     { rel: "icon", url: Favicon.src },
     { rel: "apple-touch-icon", url: AppleTouchIcon.src },
   ],
   openGraph: {
-    type: "website",
-    title: "NameGuard - Protect your community with NameGuard for ENS",
-    description:
-      "Guard your users from heartbreak and encourage best practice usage of ENS.",
-    url: "https://nameguard.io",
-    images: {
-      url: "https://nameguard.io/openGraph/og-image.png",
-      alt: "NameGuard - Protect your community with NameGuard for ENS",
-    },
+    ...NamehashMetadata.defaultMetdata.openGraph,
+    title,
+    description,
+    images: "openGraph/og-image.png",
   },
   twitter: {
-    card: "summary_large_image",
-    site: "@NamehashLabs",
-    creator: "@NamehashLabs",
-    description:
-      "Guard your users from heartbreak and encourage best practice usage of ENS.",
-    images: {
-      url: "https://nameguard.io/openGraph/og-image-twitter.png",
-      alt: "NameGuard - Protect your community with NameGuard for ENS",
-    },
+    ...NamehashMetadata.defaultMetdata.twitter,
+    description,
+    images: "openGraph/og-image-twitter.png",
   },
 };
 
