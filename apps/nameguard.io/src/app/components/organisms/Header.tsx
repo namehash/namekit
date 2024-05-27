6918e624ff9e8cba9167937be4b028350b8a4b02
eduramme <eduramme@gmail.com>
2024-05-27 18:39:31 -0300

    fix header styles

--- apps/nameguard.io/src/app/components/organisms/Header.tsx
+++ apps/nameguard.io/src/app/components/organisms/Header.tsx
@@ -8,7 +8,7 @@ import { NGSearch } from "@/app/components/molecules/NGSearch";
 export const Header = () => {
   return (
     <header className="sticky bg-white top-0 w-full z-20 border-b border-gray-300 h-[56px] py-[9px] gt_mobile:h-[70px] gt_mobile:py-4 select-none">
-      <div className="max-w-7xl mx-auto items-center justify-between gt_mobile:flex gt_mobile:flex-row inline-flex px-6">
+      <div className="max-w-7xl mx-auto items-center justify-between flex flex-row px-6">
         <div className="flex flex-row gap-7 justify-between items-center">
           <div className="flex flex-row justify-between items-center gap-1 cursor-pointer">
             <Link
@@ -25,33 +25,36 @@ export const Header = () => {
               </div>
             </Link>
           </div>
-          <div className="flex search_bar_change:block gt_mobile:w-full gt_mobile:h-full gt_mobile:p-0 gt_mobile:m-0">
+          <div className="search_bar_change:flex gt_mobile:w-full gt_mobile:h-full gt_mobile:p-0 gt_mobile:m-0 hidden">
             <NGSearch />
           </div>
         </div>
-        <div className="flex flex-row items-center justify-end gap-0 gt_mobile:justify-between gt_mobile:gap-1 md:gap-5">
-          <a
-            className="px-[10px] py-[9px] not-italic text-black font-medium text-sm leading-5 rounded-md gt_mobile:hover:bg-gray-100"
-            href="https://api.nameguard.io/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Docs
-          </a>
-          <a
-            className="rounded-md gt_mobile:hover:bg-gray-100"
-            href="https://github.com/namehash/nameguard"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex flex-row justify-between items-center gap-2 py-[7px] px-[10px]">
-              <GithubIcon className="hidden md:block text-[#0F172A] fill-current" />
-              <p className="not-italic text-black font-medium text-sm leading-5">
-                Github
-              </p>
-            </div>
-          </a>
-          <div className="flex search_bar_change:hidden flex-col justify-center align-center gap-2 p-[7px]">
+        <div className="flex flex-row items-center justify-between gap-1 md:gap-5">
+          <div className="flex items-center justify-center ">
+            <a
+              className="px-[10px] py-[9px] not-italic text-black font-medium text-sm leading-5 rounded-md gt_mobile:hover:bg-gray-100"
+              href="https://api.nameguard.io/docs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Docs
+            </a>
+            <a
+              className="rounded-md gt_mobile:hover:bg-gray-100"
+              href="https://github.com/namehash/nameguard"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="flex flex-row justify-between items-center gap-2 py-[7px] px-[10px]">
+                <GithubIcon className="hidden md:block text-[#0F172A] fill-current" />
+                <p className="not-italic text-black font-medium text-sm leading-5">
+                  Github
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="flex search_bar_change:hidden justify-center align-center p-[7px]">
             <NGSearchIcon />
           </div>
         </div>
