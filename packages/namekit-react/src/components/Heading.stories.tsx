505bae3c0745070986549863320dba63306d5b22
Jamie Barton <jamie@notrab.dev>
2024-06-11 08:45:24 +0100

    add asChild example

--- packages/namekit-react/src/components/Heading.stories.tsx
+++ packages/namekit-react/src/components/Heading.stories.tsx
@@ -1,14 +1,18 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Heading } from "./Heading";
 
 const meta: Meta<typeof Heading> = {
+  title: "UI/Heading",
   component: Heading,
-  title: "Namekit/Heading",
   argTypes: {
     as: {
-      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
-      control: { type: "select" },
+      control: {
+        type: "select",
+        options: ["h1", "h2", "h3", "h4", "h5", "h6"],
+      },
     },
+    asChild: { control: { disable: true } },
   },
 };
 
@@ -55,5 +59,13 @@ export const H6: Story = {
   args: {
     as: "h6",
     children: "Heading 6",
+  },
+};
+
+export const AsChild: Story = {
+  args: {
+    as: "h1",
+    children: "Heading as a span",
+    asChild: <span />,
   },
 };
--- packages/namekit-react/src/components/Heading.tsx
+++ packages/namekit-react/src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cc from "classcat";
 
-interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   asChild?: React.ReactElement;
 }
