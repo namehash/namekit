7fd6907a0254b151732bc1b9a34e54989eca02ad
Jamie Barton <jamie@notrab.dev>
2024-06-27 13:10:45 +0100

    feat(namekit-react): input/textarea (#290)
    * feat(namekit-react): input/textarea
    
    * styles
    
    * slots
    
    * style fixes
    
    * upgrade turborepo
    
    * build test
    
    * build test
    
    * remove styles
    
    * remove font styles
    
    * remove deps
    
    * add deps
    
    * add styles
    
    * reapply old changes

--- apps/storybook/.storybook/preview.js
+++ apps/storybook/.storybook/preview.js
@@ -6,12 +6,6 @@ import "../global.css";
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
     options: {
       storySort: {
         order: ["Nameguard", "Namekit", "ENS Utils", "ENS Webfont"],
--- packages/namekit-react/src/index.ts
+++ packages/namekit-react/src/index.ts
@@ -8,4 +8,9 @@ export * from "./components/Button";
 export * from "./components/IconButton";
 export * from "./components/Heading";
 export * from "./components/Text";
+export * from "./components/Textarea";
+export * from "./components/ENSTextarea";
+export * from "./components/Input";
+export * from "./components/ENSInput";
 export * from "./components/Link";
+
