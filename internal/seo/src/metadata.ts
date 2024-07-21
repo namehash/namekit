79ba2827f0419c03632944c0a9d0c149ec1787bf
Jamie Barton <jamie@notrab.dev>
2024-07-21 14:18:40 +0100

    remove fallback - temo

--- internal/seo/src/metadata.ts
+++ internal/seo/src/metadata.ts
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 
 export const defaultMetdata: Metadata = {
-  ...(process.env.VERCEL_PROJECT_PRODUCTION_URL && {
-    metadataBase: new URL(process.env.VERCEL_PROJECT_PRODUCTION_URL),
-  }),
   keywords: ["ens", "web3", "eth", "nameguard", "namegraph", "namekit"],
   twitter: {
     card: "summary_large_image",
@@ -12,8 +9,5 @@ export const defaultMetdata: Metadata = {
   },
   openGraph: {
     type: "website",
-    ...(process.env.VERCEL_PROJECT_PRODUCTION_URL && {
-      url: new URL(process.env.VERCEL_PROJECT_PRODUCTION_URL),
-    }),
   },
 };
