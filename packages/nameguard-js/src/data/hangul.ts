76987ab65b26c0f280437cf9a0adc497a6492401
Carbon225 <carbon225@protonmail.com>
2024-07-22 15:31:23 +0200

    improve grapheme splitting docs

---
+++ packages/nameguard-js/src/data/hangul.ts
@@ -0,0 +1,4 @@
+import HANGUL_JAMO_ from './hangul_jamo.json';
+
+// Contains all Hangul Jamo characters
+export const HANGUL_JAMO: Set<string> = new Set(HANGUL_JAMO_);
---
+++ packages/nameguard-js/src/data/invisible_joiners.ts
@@ -0,0 +1,4 @@
+import INVISIBLE_JOINERS_ from './invisible_joiners.json';
+
+// Contains invisible characters which are joined with preceding graphemes
+export const INVISIBLE_JOINERS: Set<string> = new Set(INVISIBLE_JOINERS_);
