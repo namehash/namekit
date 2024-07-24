6e4dfa96a438d653433aba789664e025d1db57c8
Carbon225 <carbon225@protonmail.com>
2024-07-24 13:17:26 +0200

    fix docstrings

--- packages/nameguard-js/src/data/hangul.ts
+++ packages/nameguard-js/src/data/hangul.ts
@@ -1,4 +1,6 @@
 import HANGUL_JAMO_ from './hangul_jamo.json';
 
-// Contains all Hangul Jamo characters
+/**
+ * Contains all Hangul Jamo characters
+ */
 export const HANGUL_JAMO: Set<string> = new Set(HANGUL_JAMO_);
