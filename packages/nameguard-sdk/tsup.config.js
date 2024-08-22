c1f81f7fdcb3164c2fa0ed309e7908b6159a8566
Jamie Barton <jamie@notrab.dev>
2024-08-22 13:55:08 +0100

    tidy

--- packages/nameguard-sdk/tsup.config.js
+++ packages/nameguard-sdk/tsup.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  external: [],
   splitting: false,
   sourcemap: true,
   clean: true,
