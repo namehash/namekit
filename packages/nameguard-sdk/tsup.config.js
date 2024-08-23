f750309f5767aaf153eb4d14365822ad100cee47
Jamie Barton <jamie@notrab.dev>
2024-08-23 07:27:11 +0100

    rollback to cjs

--- packages/nameguard-sdk/tsup.config.js
+++ packages/nameguard-sdk/tsup.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["cjs", "esm"],
   external: [],
   splitting: false,
   sourcemap: true,
