085510c8e607eec167281f3d3294098eaae0601a
Jamie Barton <jamie@notrab.dev>
2024-08-23 14:55:30 +0100

    try next one

--- packages/nameguard-sdk/tsup.config.js
+++ packages/nameguard-sdk/tsup.config.js
@@ -1,22 +1,12 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: ["src/index.ts"],
-    format: ["esm"],
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    bundle: true,
-    external: ["@namehash/ens-utils"],
-  },
-  {
-    entry: ["src/index.ts"],
-    format: ["esm"],
-    dts: {
-      only: true,
-      resolve: true,
-    },
-    external: ["@namehash/ens-utils"],
-  },
-]);
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  bundle: true,
+  dts: true,
+  external: ["@namehash/ens-utils"],
+});
