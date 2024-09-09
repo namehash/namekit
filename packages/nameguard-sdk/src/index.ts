a01ff3076986c0da36178de9d4f0589b8e20b2be
kwrobel.eth <djstrong@gmail.com>
2024-09-09 17:06:17 +0200

    Update packages/nameguard-sdk/src/index.ts
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- packages/nameguard-sdk/src/index.ts
+++ packages/nameguard-sdk/src/index.ts
@@ -682,7 +682,7 @@ export class NameGuard {
    *
    * Returns `display_name` to be shown to users and estimates `impersonation_status`
    *
-   * If `options.computeNameGuardReport` is `False`, the `nameguard_result` field will be `null`.
+   * If `address` has a primary name and `options.computeNameGuardReport` is `True`, then NameGuard will attempt to inspect that primary name. Else, NameGuard will not perform any primary name inspection and the returned  `nameguard_result` field  will be `null`.
    * 
    * @param {string} address An Ethereum address.
    * @param {SecurePrimaryNameOptions} options The options for the secure primary name.
