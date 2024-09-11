f75c738b9913caa5a68b4dba2395eac7c3f12f40
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2024-09-11 21:10:43 +0300

    Update sharing and report URL generation

--- packages/nameguard-react/src/utils/openreport.ts
+++ packages/nameguard-react/src/utils/openreport.ts
@@ -1,16 +1,16 @@
 import { ENSName } from "@namehash/ens-utils";
-import { redirectToViewNameReportURL } from "./url";
+import { redirectToReport, ReportURLGenerator } from "./url";
 
-export type OpenReportHandler = (name: ENSName) => void;
+export type OpenReportHandler = (name: ENSName, generator?: ReportURLGenerator) => void;
 
-export function defaultOpenReportHandler(name: ENSName) {
-  redirectToViewNameReportURL(name);
+export function defaultOpenReportHandler(name: ENSName, generator?: ReportURLGenerator): void {
+  redirectToReport(name, generator);
 }
 
-export function openReport(name: ENSName, handler?: OpenReportHandler) {
+export function openReport(name: ENSName, handler?: OpenReportHandler, generator?: ReportURLGenerator): void {
   if (handler) {
-    handler(name);
+    handler(name, generator);
   } else {
-    defaultOpenReportHandler(name);
+    defaultOpenReportHandler(name, generator);
   }
 }
--- packages/nameguard-react/src/utils/url.ts
+++ packages/nameguard-react/src/utils/url.ts
@@ -1,11 +1,21 @@
 import { ENSName } from "@namehash/ens-utils";
 
-export function viewNameReportURL(ensName: ENSName): URL {
+export type ReportURLGenerator = (name: ENSName) => URL;
+
+export function defaultReportURLGenerator(ensName: ENSName): URL {
   return new URL(
     `https://nameguard.io/inspect/${encodeURIComponent(ensName.name)}`,
   );
 }
 
-export function redirectToViewNameReportURL(ensName: ENSName) {
-  window.location.href = viewNameReportURL(ensName).href;
+export function getReportURL(name: ENSName, generator?: ReportURLGenerator): URL {
+  if (generator) {
+    return generator(name);
+  } else {
+    return defaultReportURLGenerator(name);
+  }
+}
+
+export function redirectToReport(ensName: ENSName, generator?: ReportURLGenerator) {
+  window.location.href = getReportURL(ensName, generator).href;
 }
