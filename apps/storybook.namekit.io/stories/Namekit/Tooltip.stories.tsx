f018b565c26bf3724358b566081559a6bdb0e0f2
FrancoAguzzi <frankind.eth@gmail.com>
2024-09-16 16:19:30 -0300

    feat: [SC-25622] Update Tooltip stories

--- apps/storybook.namekit.io/stories/Namekit/Tooltip.stories.tsx
+++ apps/storybook.namekit.io/stories/Namekit/Tooltip.stories.tsx
@@ -5,17 +5,43 @@ import { Tooltip } from "@namehash/namekit-react/client";
 const meta: Meta<typeof Tooltip> = {
   title: "UI/Tooltip",
   component: Tooltip,
+  parameters: {
+    layout: "centered",
+  },
   argTypes: {
+    trigger: {
+      control: { type: "select" },
+      options: ["Tooltip Trigger", "Any JSX element can be used as a trigger"],
+      mapping: {
+        "Tooltip Trigger": <>Tooltip Trigger</>,
+        "Any JSX element can be used as a trigger": (
+          <button disabled={true}>
+            Any JSX element can be used as a trigger
+          </button>
+        ),
+      },
+    },
+    children: {
+      control: { type: "select" },
+      options: ["Tooltip Trigger", "Any JSX element can be used as a trigger"],
+      mapping: {
+        "Tooltip content": <>Tooltip content</>,
+        "Any JSX element can be used as a content": (
+          <h1>Any JSX element can be used as a content</h1>
+        ),
+      },
+    },
     placement: {
       options: ["top", "right", "bottom", "left"],
       control: { type: "select" },
     },
-    children: {
-      control: { type: "text" },
+    maxTooltipWidth: {
+      control: { type: "number" },
     },
-    trigger: {
-      control: { type: "text" },
-    },
+  },
+  args: {
+    placement: "top",
+    maxTooltipWidth: 400,
   },
 };
 
@@ -44,6 +70,28 @@ export const LeftPlacement: Story = {
     placement: "left",
     trigger: <>Tooltip Trigger</>,
     children: <>Tooltip content</>,
+  },
+  render: (args) => {
+    return (
+      <div className="nk-w-full nk-flex nk-justify-center">
+        <Tooltip {...args} />
+      </div>
+    );
+  },
+};
+
+export const TopPlacement: Story = {
+  args: {
+    placement: "top",
+    trigger: <>Tooltip Trigger</>,
+    children: <>Tooltip content</>,
+  },
+  render: (args) => {
+    return (
+      <div className="nk-h-screen nk-flex nk-items-center">
+        <Tooltip {...args} />
+      </div>
+    );
   },
 };
 
