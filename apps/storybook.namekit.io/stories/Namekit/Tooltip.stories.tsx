2938fa91e24078fa91565233f48682279b50bab7
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2024-09-18 23:26:01 +0300

    Enhancements

--- apps/storybook.namekit.io/stories/Namekit/Tooltip.stories.tsx
+++ apps/storybook.namekit.io/stories/Namekit/Tooltip.stories.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
-import {
-  Tooltip,
-  TooltipPlacement,
-  DEFAULT_MAX_TOOLTIP_WIDTH,
-} from "@namehash/namekit-react/client";
+import { Tooltip, TooltipPlacement } from "@namehash/namekit-react/client";
 
 const meta: Meta<typeof Tooltip> = {
   title: "UI/Tooltip",
   component: Tooltip,
+  tags: ["autodocs"],
   parameters: {
     layout: "centered",
   },
   argTypes: {
-    trigger: {
-      control: false,
-    },
-    children: {
-      control: false,
-    },
     placement: {
       options: Object.values(TooltipPlacement),
       control: { type: "select" },
@@ -26,10 +17,6 @@ const meta: Meta<typeof Tooltip> = {
     maxTooltipWidth: {
       control: { type: "number" },
     },
-  },
-  args: {
-    placement: TooltipPlacement.TOP,
-    maxTooltipWidth: DEFAULT_MAX_TOOLTIP_WIDTH,
   },
 };
 
@@ -39,60 +26,88 @@ type Story = StoryObj<typeof Tooltip>;
 
 export const TopPlacement: Story = {
   args: {
-    trigger: <>Tooltip Trigger</>,
-    children: <>Tooltip content</>,
-    placement: TooltipPlacement.TOP,
+    placement: TooltipPlacement.Top,
+  },
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>Tooltip content</span>
+      </Tooltip>
+    );
   },
 };
 
 export const BottomPlacement: Story = {
   args: {
-    trigger: <>Tooltip Trigger</>,
-    children: <>Tooltip content</>,
-    placement: TooltipPlacement.BOTTOM,
+    placement: TooltipPlacement.Bottom,
+  },
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>Tooltip content</span>
+      </Tooltip>
+    );
   },
 };
 
 export const LeftPlacement: Story = {
   args: {
-    trigger: <>Tooltip Trigger</>,
-    children: <>Tooltip content</>,
-    placement: TooltipPlacement.LEFT,
+    placement: TooltipPlacement.Left,
+  },
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>Tooltip content</span>
+      </Tooltip>
+    );
   },
 };
 
 export const RightPlacement: Story = {
   args: {
-    trigger: <>Tooltip Trigger</>,
-    children: <>Tooltip content</>,
-    placement: TooltipPlacement.RIGHT,
+    placement: TooltipPlacement.Right,
+  },
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>Tooltip content</span>
+      </Tooltip>
+    );
   },
 };
 
 export const DefaultMaxTooltipWidth: Story = {
-  args: {
-    trigger: <>Tooltip Trigger</>,
-    children: (
-      <>
-        This is the default max tooltip width. You can customize the max width
-        if desired. See the "Custom Max Tooltip Width" story for an example of
-        setting a custom max width.
-      </>
-    ),
-    placement: TooltipPlacement.BOTTOM,
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>
+          This is the default max tooltip width. You can customize the max width
+          if desired. See the "Custom Max Tooltip Width" story for an example of
+          setting a custom max width.
+        </span>
+      </Tooltip>
+    );
   },
 };
 
 export const CustomMaxTooltipWidth: Story = {
   args: {
-    trigger: <>Tooltip Trigger</>,
-    children: (
-      <>
-        This is a customized max tooltip width. You can customize the max width
-        if desired.
-      </>
-    ),
-    placement: TooltipPlacement.BOTTOM,
     maxTooltipWidth: 200,
+  },
+  render: (args) => {
+    const trigger = <span>Tooltip Trigger</span>;
+    return (
+      <Tooltip {...args} trigger={trigger}>
+        <span>
+          This is a customized max tooltip width. You can customize the max
+          width if desired.
+        </span>
+      </Tooltip>
+    );
   },
 };
