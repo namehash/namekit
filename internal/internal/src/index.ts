152c8f00106284374c8242b45c26d122a91a7306
eduramme <eduramme@gmail.com>
2024-10-20 00:32:21 -0300

    reaname CalendarButotn file for consistency

--- internal/internal/src/index.ts
+++ internal/internal/src/index.ts
@@ -6,4 +6,4 @@ export { ContactUsForm } from "./contact-form/contact-form";
 export { validateContactFormData } from "./contact-form/validation";
 export { type ContactFormDataProps } from "./contact-form/types";
 
-export { CalendarButton } from "./CalendarButton";
+export { CalendarButton } from "./calendar-button";
