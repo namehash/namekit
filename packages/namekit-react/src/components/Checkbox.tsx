a9e4ea5bf2f69500dc91a7f59d6596a40afd29a2
eduramme <eduramme@gmail.com>
2024-10-29 18:58:39 -0300

    feat: update Checkbox style

--- packages/namekit-react/src/components/Checkbox.tsx
+++ packages/namekit-react/src/components/Checkbox.tsx
@@ -4,7 +4,7 @@ export const Checkbox = (props: InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <input
       type="checkbox"
-      className="nk-form-checkbox nk-h-4 nk-w-4 nk-rounded !nk-border-black nk-text-black !nk-ring-black"
+      className="nk-form-checkbox nk-h-4 nk-w-4 nk-rounded nk-border-[1.25px] nk-border-gray-300 nk-text-black nk-ring-0 nk-cursor-pointer checked:nk-ring-0 focus:nk-ring-0 focus:nk-ring-transparent focus:nk-outline-none focus-visible:nk-border-gray-400 active:nk-ring-0 hover:nk-border-gray-400"
       {...props}
     />
   );
