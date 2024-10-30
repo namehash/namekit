1a4637d7abb802a18341177c7200811d1057f1e8
eduramme <eduramme@gmail.com>
2024-10-30 11:01:26 -0300

    feat: remove hover effects when disabled

--- packages/namekit-react/src/components/Checkbox.tsx
+++ packages/namekit-react/src/components/Checkbox.tsx
@@ -7,7 +7,7 @@ export const Checkbox = (props: InputHTMLAttributes<HTMLInputElement>) => {
       type="checkbox"
       {...props}
       className={cc([
-        "nk-form-checkbox nk-h-4 nk-w-4 nk-rounded nk-border-[1.25px] nk-border-gray-300 nk-text-black nk-ring-0 nk-cursor-pointer checked:nk-ring-0 focus:nk-ring-0 focus:nk-ring-transparent focus:nk-outline-none focus-visible:nk-border-gray-400 active:nk-ring-0 hover:nk-border-gray-400 disabled:nk-cursor-not-allowed disabled:nk-opacity-50 disabled:nk-bg-gray-100 disabled:checked:nk-bg-gray-300",
+        "nk-form-checkbox nk-h-4 nk-w-4 nk-rounded nk-border-[1.25px] nk-border-gray-300 nk-text-black nk-ring-0 nk-cursor-pointer checked:nk-ring-0 focus:nk-ring-0 focus:nk-ring-transparent focus:nk-outline-none focus-visible:nk-border-gray-400 active:nk-ring-0 enabled:hover:nk-border-gray-400 disabled:nk-cursor-not-allowed disabled:nk-opacity-50 disabled:nk-bg-gray-100 disabled:checked:nk-bg-gray-300",
         props.className,
       ])}
     />
