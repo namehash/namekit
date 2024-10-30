f40de57964ca9bf3086c3ab07c0e18f4c770cdd2
eduramme <eduramme@gmail.com>
2024-10-30 11:01:01 -0300

    feat: disable hover effects when disabled

---
+++ apps/storybook.namekit.io/stories/Namekit/Checkbox.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "@namehash/namekit-react";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "UI/Checkbox",
+  component: Checkbox,
+  argTypes: {
+    disabled: { control: "boolean" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const DisabledChecked: Story = {
+  args: {
+    checked: true,
+    disabled: true,
+  },
+};
+
+export const CustomStyling: Story = {
+  args: {
+    style: {
+      width: "24px",
+      height: "24px",
+      backgroundColor: "blue",
+      borderWidth: "2px",
+      borderColor: "red",
+      transition: "all 0.3s ease",
+    },
+  },
+};
