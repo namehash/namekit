eed1ed0ea8a6f3b06d486c9f8fe892c1a61b1962
eduramme <eduramme@gmail.com>
2024-11-08 23:43:29 -0300

    fix DevCon date

--- apps/namehashlabs.org/components/1 - atoms/headline-banner.tsx
+++ apps/namehashlabs.org/components/1 - atoms/headline-banner.tsx
@@ -7,7 +7,7 @@ export const HeadlineBanner = () => {
     <div className="px-5 text-white justify-center items-center w-full bg-[#3B0D48] space-x-4 py-4 text-sm relative z-20 flex">
       <div className="flex space-x-3 items-center">
         <MegaphoneIcon className="flex shrink-0 w-6 h-6 opacity-50 -mr-1" />
-        <p className=" sm:pr-0">Meet us at DevCon in Bangkok 4-7 Nov 2024</p>
+        <p className=" sm:pr-0">Meet us at DevCon in Bangkok 12-15 Nov 2024</p>
       </div>
 
       <CalendarButton
