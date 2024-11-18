19aec6a49784d5e8c42f791809e55860c83da4e0
eduramme <eduramme@gmail.com>
2024-11-18 17:19:27 -0300

    use Headings on "the vision" section

--- apps/namehashlabs.org/components/2 - molecules/the-vision-section.tsx
+++ apps/namehashlabs.org/components/2 - molecules/the-vision-section.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Balancer } from "react-wrap-balancer";
-import { PreSectionText, SectionText, SectionTitle } from "../1 - atoms";
+import { PreSectionText } from "@/components/1 - atoms";
 import Lottie from "lottie-react";
 import { useEffect, useState } from "react";
+import { Heading, Text } from "@namehash/namekit-react";
 
 export const TheVisionSection = () => {
   const [errorLoadingAnimation, setErrorLoadingAnimation] = useState(false);
@@ -28,16 +29,18 @@ export const TheVisionSection = () => {
     >
       <div className="flex flex-col items-center justify-center gap-3 mb-[60px] lg:mx-10 md:pb-10">
         <PreSectionText>THE VISION</PreSectionText>
-        <SectionTitle>
+
+        <Heading as="h1">
           <Balancer>
             Bring ENS to millions of people where they already are
           </Balancer>
-        </SectionTitle>
-        <SectionText className="text-center">
+        </Heading>
+
+        <Text as="p" className="text-lg text-gray-500">
           <Balancer>
             Directly embed ENS onboarding into every wallet, app, and game.
           </Balancer>
-        </SectionText>
+        </Text>
       </div>
       <Lottie
         className="w-[460px] h-[204px] xSmall:w-[552px] xSmall:h-[245px] small:w-[668px] small:h-[297px] sm:w-[735px] sm:h-[327px] md:w-[764px] md:h-[339px] medium2x:w-[801px] medium2x:h-[355px] lg:!w-[984px] lg:!h-[437px] xl:!w-[1400px] xl:!h-[622px]"
