f10f837f7f963f9771104436c21709bbeaa27ef4
caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>
2024-12-05 20:22:40 -0300

    feat: [SC-25948] :sparkles: Add new endpoint to NameGraph: sampleCollectionMembers (#480)

--- packages/namegraph-sdk/src/index.ts
+++ packages/namegraph-sdk/src/index.ts
@@ -193,6 +193,23 @@ export class NameGraph {
     return this.rawRequest(`grouped-by-category`, "POST", payload);
   }
 
+  public sampleCollectionMembers(
+    collection_id: string,
+  ): Promise<NameGraphSuggestion[]> {
+    const metadata = true;
+    const max_sample_size = 5;
+    const seed = 5;
+
+    const payload = {
+      collection_id,
+      metadata,
+      max_sample_size,
+      seed,
+    };
+
+    return this.rawRequest("sample_collection_members", "POST", payload);
+  }
+
   public findCollectionsByString(
     query: string,
   ): Promise<FindCollectionsResponse> {
