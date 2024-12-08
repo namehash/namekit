b0d2373c57ae90f6093b8e35a27c5074ea283108
eduramme <eduramme@gmail.com>
2024-12-08 17:00:25 -0300

    fix .ico image error

--- apps/namekit.io/src/app/layout.tsx
+++ apps/namekit.io/src/app/layout.tsx
@@ -28,28 +28,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-
         {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
