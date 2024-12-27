529bb9bf8195e654d9b28614a8c367b21b159197
eduramme <eduramme@gmail.com>
2024-12-27 19:15:33 -0300

    fix footer

--- apps/namekit.io/src/components/organisms/namekit-footer.tsx
+++ apps/namekit.io/src/components/organisms/namekit-footer.tsx
@@ -4,8 +4,8 @@ import { EnvelopeIcon } from "@heroicons/react/24/solid";
 export const NamekitFooter = () => {
   return (
     <footer className="px-6 border-t border-gray-200 pt-5 pb-4 md:pb-[18px] md:pt-6">
-      <div className="flex flex-col space-y-7 md:space-y-0 items-start md:items-center md:flex-row max-w-[1216px] mx-auto justify-between">
-        <div className="w-full md:w-auto inline-flex items-center justify-between">
+      <div className="flex flex-col space-y-7 md:space-y-0 items-center md:items-center md:flex-row max-w-[1216px] mx-auto justify-between">
+        <div className="w-full md:w-auto inline-flex items-center justify-center">
           <p className="text-xs text-gray-500">
             &copy; NameHash Labs. All Rights Reserved
           </p>
