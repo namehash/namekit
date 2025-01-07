a0967d9ac13ee060f0112f66f27a6a1ff06e7328
Jamie Barton <jamie@notrab.dev>
2025-01-07 14:00:58 +0000

    add min/max variables

--- apps/namerank.io/app/indicator.tsx
+++ apps/namerank.io/app/indicator.tsx
@@ -1,8 +1,8 @@
 export const calculateNormalizedPercentage = (
-  logProbability: number,
+  log_probability: number,
 ): number => {
   // Ensure value is within bounds
-  const clampedValue = Math.max(-100, Math.min(0, logProbability));
+  const clampedValue = Math.max(-100, Math.min(0, log_probability));
 
   // Convert -100...0 range to 0...1 range
   return (clampedValue + 100) / 100;
@@ -26,7 +26,9 @@ export const Indicator = ({ log_probability }: IndicatorProps) => {
   const normalizedPercentage = calculateNormalizedPercentage(log_probability);
   const [r, g, b] = getRGBColor(normalizedPercentage);
 
-  const width = Math.max(50, 128 * normalizedPercentage);
+  const minWidth = 20;
+  const maxWidth = 128;
+  const width = Math.max(minWidth, maxWidth * normalizedPercentage);
 
   return (
     <div
