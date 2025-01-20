23ce8d7f4cda360db4b6121febe87e4640b50bf9
kwrobel.eth <djstrong@gmail.com>
2025-01-20 13:12:36 +0100

    Set fetchCollectionMembers limit (#555)
    * Set fetchCollectionMembers limit
    
    * larger height
    
    * max-h?
    
    * feat: [SC-26100] :recycle: Remove automatic seed generation from NameGraph SDK
    
    * fix: :bug: Update Ui rendering in Collection Details page for collection names box size
    
    * Add changeset
    
    ---------
    
    Co-authored-by: FrancoAguzzi <frankind.eth@gmail.com>
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- apps/namegraph.dev/app/collections/[id]/explore-collection-page.tsx
+++ apps/namegraph.dev/app/collections/[id]/explore-collection-page.tsx
@@ -114,6 +114,7 @@ export const ExploreCollectionPage = ({ id }: { id: string }) => {
     setLoadingCollectionMembers(true);
     fetchCollectionMembers(id, {
       offset: (params.page - 1) * navigationConfig.itemsPerPage,
+      limit: navigationConfig.itemsPerPage,
     })
       .then((res) => {
         if (res) {
@@ -313,7 +314,7 @@ export const ExploreCollectionPage = ({ id }: { id: string }) => {
                     <div className="w-full">
                       <div className="w-full mb-8">
                         <div className="w-full flex flex-col space-y-4 p-3 rounded-xl border border-gray-200">
-                          <div className="w-full h-[517px] flex flex-col justify-start">
+                          <div className="w-full h-[877px] flex flex-col justify-start">
                             <div className="h-full">
                               {/* Collection Count and Sort */}
                               <div className="max-w-[756px] w-full flex flex-col md:flex-row md:items-center md:justify-between space-y-4 md:space-y-0 mb-5">
