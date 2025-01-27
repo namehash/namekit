28ac8eeec6b1503aa0eaa027a449517820706fd2
Jamie Barton <jamie@notrab.dev>
2025-01-27 16:37:51 +0000

    fix rank form errors

--- apps/nameai.dev/app/tokenization/rank-form.tsx
+++ apps/nameai.dev/app/tokenization/rank-form.tsx
@@ -32,16 +32,22 @@ export function RankForm() {
 
   useEffect(() => {
     if (inputValue) {
-      if (inputValue.includes(".")) {
-        setClientError("Enter only 1 label (no '.')");
+      let error = null;
+
+      if (inputValue.includes(" ")) {
+        error = "Remove spaces from the label";
+      } else if (inputValue.includes(".")) {
+        error = "Enter only 1 label (no '.')";
       } else {
         try {
           ens_normalize(inputValue);
           setClientError(null);
         } catch (error) {
-          setClientError("Please enter a valid ENS label value");
+          error = "Enter a valid ENS label value";
         }
       }
+
+      setClientError(error);
     } else {
       setClientError(null);
     }
@@ -53,7 +59,7 @@ export function RankForm() {
   return (
     <>
       <form action={formAction} className="mb-6">
-        <div className="flex items-center justify-center space-x-2 flex-1">
+        <div className="flex items-start justify-center space-x-2 flex-1">
           <Input
             type="text"
             name="label"
@@ -64,18 +70,18 @@ export function RankForm() {
             onChange={(e) => setInputValue(e.target.value)}
             autoComplete="off"
             data-1p-ignore
+            error={clientError ?? ""}
           />
-          <Button type="submit" disabled={isSubmitDisabled}>
+          <Button type="submit" disabled={isSubmitDisabled} className="!py-1.5">
             {isPending ? "Analyzing..." : "Analyze"}
           </Button>
         </div>
-        {clientError && <p className="text-red-500 mt-2">{clientError}</p>}
-        {state.error && <p className="text-red-500 mt-2">{state.error}</p>}
       </form>
 
       {isPending && <Skeleton label={inputValue} />}
 
       {!isPending && state.success && state.label && state.analysis && (
+        // @ts-expect-error
         <Results analysis={state.analysis} />
       )}
     </>
