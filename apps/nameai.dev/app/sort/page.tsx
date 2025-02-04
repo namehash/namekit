538fd66553313f484b4274cda4cb1e9e1de3300d
eduramme <eduramme@gmail.com>
2025-02-04 19:01:52 -0300

    Add "Pretty" background illustrations to demo apps

--- apps/nameai.dev/app/sort/page.tsx
+++ apps/nameai.dev/app/sort/page.tsx
@@ -33,15 +33,28 @@ export default async function SortPage() {
   const initialLabels = await getInitialLabelsItems();
 
   return (
-    <div className="max-w-3xl mx-auto px-6 flex-1 py-12 md:py-20">
-      <div className="text-center mb-6 md:mb-12 space-y-3">
-        <Heading as="h1">AI Sort</Heading>
-        <Text as="p" className="text-gray-500">
-          Bring names more likely to be interesting to the top.
-        </Text>
-      </div>
+    <div className="relative bg-blue">
+      <div
+        className="absolute top-0 left-0 w-full h-full z-0"
+        style={{
+          width: "100vw",
+          height: "472px",
+          flexShrink: 0,
+          opacity: 0.15,
+          background: `linear-gradient(180deg, rgba(249, 250, 251, 0.00) 0%, #F9FAFB 74.87%), 
+                    linear-gradient(90deg, #FFAF00 0%, #F112D9 32.29%, #4C3FA0 70.83%, #2ED3C6 95.83%)`,
+        }}
+      />
+      <div className="max-w-3xl mx-auto px-6 flex-1 py-12 md:py-20">
+        <div className="text-center mb-6 md:mb-12 space-y-3">
+          <Heading as="h1">AI Sort</Heading>
+          <Text as="p" className="text-gray-500">
+            Bring names more likely to be interesting to the top.
+          </Text>
+        </div>
 
-      <Client initialLabels={initialLabels} />
+        <Client initialLabels={initialLabels} />
+      </div>
     </div>
   );
 }
--- apps/nameai.dev/app/tokenization/page.tsx
+++ apps/nameai.dev/app/tokenization/page.tsx
@@ -18,18 +18,31 @@ export default async function TokenizePage(props: Props) {
   const label = searchParams?.label || "";
 
   return (
-    <div className="max-w-3xl mx-auto px-6 flex-1 py-12 md:py-20">
-      <div className="text-center mb-6 md:mb-12 space-y-3">
-        <Heading as="h1">Label Tokenization</Heading>
-        <Text as="p" className="text-gray-500 text-balance">
-          <Balancer>
-            Extract recognizable words that are otherwise &quot;smashed
-            together&quot; in labels.
-          </Balancer>
-        </Text>
-      </div>
+    <div className="relative bg-blue">
+      <div
+        className="absolute top-0 left-0 w-full h-full"
+        style={{
+          width: "100vw",
+          height: "472px",
+          flexShrink: 0,
+          opacity: 0.15,
+          background: `linear-gradient(180deg, rgba(249, 250, 251, 0.00) 0%, #F9FAFB 74.87%), 
+                    linear-gradient(90deg, #FFAF00 0%, #F112D9 32.29%, #4C3FA0 70.83%, #2ED3C6 95.83%)`,
+        }}
+      />
+      <div className="max-w-3xl mx-auto px-6 flex-1 py-12 md:py-20">
+        <div className="text-center mb-6 md:mb-12 space-y-3">
+          <Heading as="h1">Label Tokenization</Heading>
+          <Text as="p" className="text-gray-500 text-balance">
+            <Balancer>
+              Extract recognizable words that are otherwise &quot;smashed
+              together&quot; in labels.
+            </Balancer>
+          </Text>
+        </div>
 
-      <Form initialValue={label} />
+        <Form initialValue={label} />
+      </div>
     </div>
   );
 }
