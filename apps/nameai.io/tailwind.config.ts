fd1b64a3781c0c5a4110f87a9be3215eddff3b57
eduramme <eduramme@gmail.com>
2025-02-14 16:36:13 -0300

    add internal footer into nameai.io

--- apps/nameai.io/tailwind.config.ts
+++ apps/nameai.io/tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@namehash/internal/src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
