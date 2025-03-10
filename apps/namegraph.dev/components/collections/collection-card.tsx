3c704bae2a32633fa619d24adfa471c3a465429a
caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>
2025-03-10 14:31:28 +0100

    Franco Aguzzi [SC-26313] Improve NameGraph dev (#621)
    * feat: [SC-26313] ✨ Improving namegraph.dev
    
    * feat: [SC-26313] ✨ Added new Ui components to namegraph.dev, fixed bugs and layout shifts
    
    * fix: [SC-26313] 🐛 Fix build error
    
    * feat: [SC-26313] 🐛  Fix navigation in results search of namegraph.dev
    
    * feat: [SC-26313] ✨ Strategically decouple collections results components into react hook and Ui components to be reused in namegraph.dev
    
    * feat: [SC-26313] 💄 Refine Ui of collections results tab switcher
    
    * feat: [SC-26313] ✨ Fix ByMembership results displaying when page is set in URL

--- apps/namegraph.dev/components/collections/collection-card.tsx
+++ apps/namegraph.dev/components/collections/collection-card.tsx
@@ -1,8 +1,10 @@
 import { NameGraphCollection } from "@namehash/namegraph-sdk/utils";
 import { Link } from "@namehash/namekit-react";
 import { Noto_Emoji } from "next/font/google";
-import { NameWithCurrentTld } from "./name-with-current-tld";
 import { getNameDetailsPageHref } from "@/lib/utils";
+import { NameWithCurrentTld, useQueryParams } from "../use-query-params";
+import { DisplayedName } from "@namehash/nameguard-react";
+import { buildENSName } from "@namehash/ens-utils";
 
 const notoBlack = Noto_Emoji({ preload: false });
 
@@ -11,6 +13,8 @@ export const CollectionCard = ({
 }: {
   collection: NameGraphCollection;
 }) => {
+  const { params } = useQueryParams();
+
   return (
     <div
       key={collection.collection_id}
@@ -46,7 +50,11 @@ export const CollectionCard = ({
                 href={getNameDetailsPageHref(tag.label)}
                 className="max-h-[28px] w-max bg-gray-100 !text-sm px-2 py-1 bg-muted rounded-full"
               >
-                <NameWithCurrentTld name={tag.label} />
+                <DisplayedName
+                  name={buildENSName(
+                    NameWithCurrentTld({ name: tag.label, params }),
+                  )}
+                />
               </Link>
             ))}
           </div>
