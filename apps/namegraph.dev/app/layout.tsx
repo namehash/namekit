e82a5a98dc1b50fa3d786af152ca578e04dc7ffb
eduramme <eduramme@gmail.com>
2025-03-19 14:47:15 -0300

    feature: refine how dev demo label is displayed

--- apps/namegraph.dev/app/layout.tsx
+++ apps/namegraph.dev/app/layout.tsx
@@ -34,18 +34,18 @@ export default function RootLayout({
             <header className="sticky bg-white top-[-2px] w-full z-50 border-b border-gray-300 h-[56px] py-[9px] sm:h-[70px] sm:py-4 select-none max-w-[100vw] overflow-hidden">
               <div className="max-w-7xl mx-auto items-center justify-between flex flex-row px-6">
                 <div className="hidden md:flex flex-row justify-between items-center">
-                  <div className="flex flex-row justify-between items-center gap-1 cursor-pointer flex-shrink-0 pr-1">
+                  <div className="flex flex-row justify-center items-center gap-1 cursor-pointer flex-shrink-0 pr-1">
                     <a
                       href="/"
-                      className="text-black flex flex-row items-center gap-1 not-italic font-bold text-[22.683px] leading-[22.683px] tracking-[-0.907px] sm:text-[27.816px] sm:leading-[27.816px] sm:tracking-[-1.113px]"
+                      className="text-black flex mr-1 flex-row items-center gap-1 not-italic font-bold text-[22.683px] leading-[22.683px] tracking-[-0.907px] sm:text-[27.816px] sm:leading-[27.816px] sm:tracking-[-1.113px]"
                     >
                       <NameGraphIcon className="w-6 h-6 mr-1" />
                       NameGraph
                     </a>
                   </div>
                   <NextLink href="/">
-                    <div className="relative -top-1.5 bg-black w-fit h-fit p-[2.8px] rounded-[2.8px] flex-shrink-0">
-                      <p className="text-white text-center not-italic font-semibold pb-[0.5px] text-[6.857px] leading-[7.619px] sm:text-[8.409px] sm:leading-[9.343px]">
+                    <div className="relative bg-white border border-black w-fit h-fit p-[2.8px] rounded-[2.8px] flex-shrink-0">
+                      <p className="text-black text-center not-italic font-semibold pb-[0.5px] text-[6.857px] leading-[7.619px] sm:text-[8.409px] sm:leading-[9.343px]">
                         dev demo
                       </p>
                     </div>
