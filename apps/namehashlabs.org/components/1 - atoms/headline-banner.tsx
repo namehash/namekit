19f2ee7b8ffd532e35f5f057e402666764ecff63
eduramme <eduramme@gmail.com>
2025-03-31 14:33:51 -0300

    Eth Denver -> Eth Dubai

--- apps/namehashlabs.org/components/1 - atoms/headline-banner.tsx
+++ apps/namehashlabs.org/components/1 - atoms/headline-banner.tsx
@@ -1,13 +1,16 @@
 import { MegaphoneIcon } from "@heroicons/react/24/outline";
 import { CalendarButton } from "@namehash/internal";
 import { Link } from "@namehash/namekit-react";
+import Balancer from "react-wrap-balancer";
 
 export const HeadlineBanner = () => {
   return (
     <div className="px-5 text-white justify-center items-center sm:h-[44px] w-full bg-[#3B0D48] space-x-1 py-4 text-sm relative z-20 flex">
       <div className="flex space-x-3 items-center">
         <MegaphoneIcon className="flex shrink-0 w-6 h-6 opacity-50 -mr-1" />
-        <p className=" sm:pr-0">Meet us at ETHDenver Feb 27 - Mar 2</p>
+        <p className=" sm:pr-0">
+          <Balancer>Meet us at ETHDubai April 28 - 29</Balancer>
+        </p>
       </div>
 
       <CalendarButton
