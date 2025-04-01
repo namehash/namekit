34d47bb3539590948d65d516d1ed05e31c5cbb58
Eduardo <eduramme@gmail.com>
2025-04-01 03:30:33 -0300

    feat: [sc-26430] Storytelling Panels Namegraph Fix (#689)
    * refactor: remove ENS vision logo
    
    * reduce panel max width and improve text distribution on mobile screens
    
    * optimize panel for mobile screens

--- apps/namegraph.dev/components/homepage/homepage.tsx
+++ apps/namegraph.dev/components/homepage/homepage.tsx
@@ -117,23 +117,29 @@ export const HomePage = () => {
           </div>
         </div>
         {/* Integration Example Panel */}
-        <div className="w-full px-5 z-20 mx-auto mt-10 mb-2">
-          <div className="w-full bg-black max-w-[1216px] mx-auto flex-col sm:flex-row gap-4 rounded-lg px-8 py-4 flex items-center justify-between justify-self-start">
-            <div className="flex items-center gap-4">
-              <div className="p-4 rounded-full bg-gray-700">
+        <div className="w-full z-20 mx-auto mt-10 mb-2 px-4 sm:px-0">
+          <div className="w-full bg-black max-w-5xl mx-auto flex-col sm:flex-row gap-6 sm:gap-4 rounded-lg p-6 sm:px-8 sm:py-4 flex items-center justify-between">
+            <div className="flex items-center gap-4 w-full sm:w-auto">
+              <div className="p-3 sm:p-4 rounded-full bg-gray-700 shrink-0">
                 <BoltIcon className="w-5 h-5 text-white" />
               </div>
 
-              <div className="flex flex-col">
+              <div className="flex flex-col min-w-0">
                 <span className="text-white text-base font-semibold">
-                  Example ENS ecosystem integration{" "}
+                  <Balancer>Example ENS ecosystem integration</Balancer>
                 </span>
                 <span className="text-gray-400 text-sm">
-                  powering related name suggestions at Vision{" "}
+                  <Balancer>
+                    powering related name suggestions at Vision{" "}
+                  </Balancer>
                 </span>
               </div>
             </div>
-            <Button variant="secondary" asChild>
+            <Button
+              variant="secondary"
+              className="w-full items-center justify-center sm:w-auto text-center"
+              asChild
+            >
               <NextLink
                 target="_blank"
                 href="https://x.com/ensvision/status/1902711055802933619"
