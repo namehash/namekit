0fd3bf843fd61e0f4e91e86b8a546d4c44563a4b
y3drk <ziebura.jedrzej@gmail.com>
2025-04-14 14:23:30 +0200

    integrating new package into: nameguard, namehashlabs, namekit and namegraph LPs

--- apps/namegraph.dev/tailwind.config.ts
+++ apps/namegraph.dev/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@namehash/internal/src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@namehash/namehashlabs-react/src/**/*.{js,jsx,ts,tsx}",
   ],
   safelist: ["rounded-big", "rounded-biggest"],
   theme: {
