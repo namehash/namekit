2ded1ff327c28cde9c3aee3df1fb68d1915c7dd6
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2026-05-08 00:25:31 +0400

    keep ready to get started section

--- apps/namekit.io/src/components/pages/namekit.tsx
+++ apps/namekit.io/src/components/pages/namekit.tsx
@@ -2,6 +2,7 @@
 
 import {
   BuildUiSection,
+  GetYourWeb3NameSection,
   HeroSection,
   IntroducingNamekit,
   TheVisionSection,
@@ -30,12 +31,12 @@ export const NameKitPage = () => {
       <IntroducingNamekit />
       <BuildUiSection />
       {/* <ServicesSection />
-      <BuildFutureSection />
+      <BuildFutureSection /> */}
       <GetYourWeb3NameSection
         title="Ready to get started?"
         description="Our team is standing by to answer your questions and find the right solution for you"
         buttonText="Schedule a call"
-      /> */}
+      />
     </div>
   );
 };
